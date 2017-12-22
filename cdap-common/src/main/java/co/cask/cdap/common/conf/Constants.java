cdapio/cdap

CDAP-12570 Add configuration to skip task level metrics emission

cdap-common/src/main/java/co/cask/cdap/common/conf/Constants.java
@@ -612,6 +612,7 @@ public final class Constants {
                       Constants.Metrics.Tag.COMPONENT, Constants.Service.TRANSACTION);
 
     public static final String EMIT_PROGRAM_CONTAINER_METRICS = "app.program.metrics.enabled";
+    public static final String EMIT_MR_TASKL_LEVEL_METRICS = "mapreduce.task.metrics.enabled";
 
     public static final String METRICS_MIGRATION_SLEEP_MILLIS = "metrics.table.migration.sleep.millis";
 

adityadivami/cdap-ui-forked

cdap-common/src/main/java/co/cask/cdap/common/conf/Constants.java
@@ -561,7 +561,7 @@ public final class Constants {
       ImmutableMap.of(Constants.Metrics.Tag.NAMESPACE, NamespaceId.SYSTEM.getNamespace(),
                       Constants.Metrics.Tag.COMPONENT, Constants.Service.METRICS_PROCESSOR);
 
-    public static final String EMIT_PRGOGRAM_CONTAINER_METRICS = "app.program.metrics.enabled";
+    public static final String PROGRAM_METRICS_ENABLED = "app.program.metrics.enabled";
 
     /**
      * Metric's dataset related constants.