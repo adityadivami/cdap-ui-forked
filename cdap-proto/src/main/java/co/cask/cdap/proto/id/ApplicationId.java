cdapio/cdap

CDAP-13088 Introduce MetadataEntity which is compatible with EntityId

cdap-proto/src/main/java/co/cask/cdap/proto/id/ApplicationId.java
@@ -15,6 +15,7 @@
  */
 package co.cask.cdap.proto.id;
 
+import co.cask.cdap.api.metadata.MetadataEntity;
 import co.cask.cdap.proto.Id;
 import co.cask.cdap.proto.ProgramType;
 import co.cask.cdap.proto.element.EntityType;
@@ -61,6 +62,12 @@ public class ApplicationId extends NamespacedEntityId implements ParentedId<Name
   @Override
   public String getEntityName() {
     return getApplication();
+  }
+
+  @Override
+  public MetadataEntity toMetadataEntity() throws UnsupportedOperationException {
+    return MetadataEntity.ofNamespace(namespace).of(MetadataEntity.APPLICATION, application)
+      .of(MetadataEntity.VERSION, version);
   }
 
   @Override

cdap-proto/src/main/java/co/cask/cdap/proto/id/ArtifactId.java
@@ -17,6 +17,7 @@ package co.cask.cdap.proto.id;
 
 import co.cask.cdap.api.artifact.ArtifactScope;
 import co.cask.cdap.api.artifact.ArtifactVersion;
+import co.cask.cdap.api.metadata.MetadataEntity;
 import co.cask.cdap.proto.Id;
 import co.cask.cdap.proto.element.EntityType;
 
@@ -88,6 +89,12 @@ public class ArtifactId extends NamespacedEntityId implements ParentedId<Namespa
   @Override
   public String getEntityName() {
     return getArtifact();
+  }
+
+  @Override
+  public MetadataEntity toMetadataEntity() throws UnsupportedOperationException {
+    return MetadataEntity.ofNamespace(namespace).of(MetadataEntity.ARTIFACT, artifact)
+      .of(MetadataEntity.VERSION, version);
   }
 
   public String getVersion() {

cdap-proto/src/main/java/co/cask/cdap/proto/id/StreamId.java
@@ -16,6 +16,7 @@
 package co.cask.cdap.proto.id;
 
 import co.cask.cdap.api.common.Bytes;
+import co.cask.cdap.api.metadata.MetadataEntity;
 import co.cask.cdap.proto.Id;
 import co.cask.cdap.proto.element.EntityType;
 
@@ -48,6 +49,11 @@ public class StreamId extends NamespacedEntityId implements ParentedId<Namespace
   @Override
   public String getEntityName() {
     return getStream();
+  }
+
+  @Override
+  public MetadataEntity toMetadataEntity() throws UnsupportedOperationException {
+    return MetadataEntity.ofNamespace(namespace).of(MetadataEntity.STREAM, stream);
   }
 
   @Override

adityadivami/cdap-ui-forked

cdap-proto/src/main/java/co/cask/cdap/proto/id/ApplicationId.java
@@ -15,6 +15,7 @@
  */
 package co.cask.cdap.proto.id;
 
+import co.cask.cdap.api.metadata.MetadataEntity;
 import co.cask.cdap.proto.Id;
 import co.cask.cdap.proto.ProgramType;
 import co.cask.cdap.proto.element.EntityType;
@@ -61,6 +62,12 @@ public class ApplicationId extends NamespacedEntityId implements ParentedId<Name
   @Override
   public String getEntityName() {
     return getApplication();
+  }
+
+  @Override
+  public MetadataEntity toMetadataEntity() throws UnsupportedOperationException {
+    return MetadataEntity.ofNamespace(namespace).append(MetadataEntity.APPLICATION, application)
+      .append(MetadataEntity.VERSION, version);
   }
 
   @Override

cdap-proto/src/main/java/co/cask/cdap/proto/id/ArtifactId.java
@@ -17,6 +17,7 @@ package co.cask.cdap.proto.id;
 
 import co.cask.cdap.api.artifact.ArtifactScope;
 import co.cask.cdap.api.artifact.ArtifactVersion;
+import co.cask.cdap.api.metadata.MetadataEntity;
 import co.cask.cdap.proto.Id;
 import co.cask.cdap.proto.element.EntityType;
 
@@ -88,6 +89,12 @@ public class ArtifactId extends NamespacedEntityId implements ParentedId<Namespa
   @Override
   public String getEntityName() {
     return getArtifact();
+  }
+
+  @Override
+  public MetadataEntity toMetadataEntity() throws UnsupportedOperationException {
+    return MetadataEntity.ofNamespace(namespace).append(MetadataEntity.ARTIFACT, artifact)
+      .append(MetadataEntity.VERSION, version);
   }
 
   public String getVersion() {

cdap-proto/src/main/java/co/cask/cdap/proto/id/StreamId.java
@@ -16,6 +16,7 @@
 package co.cask.cdap.proto.id;
 
 import co.cask.cdap.api.common.Bytes;
+import co.cask.cdap.api.metadata.MetadataEntity;
 import co.cask.cdap.proto.Id;
 import co.cask.cdap.proto.element.EntityType;
 
@@ -48,6 +49,11 @@ public class StreamId extends NamespacedEntityId implements ParentedId<Namespace
   @Override
   public String getEntityName() {
     return getStream();
+  }
+
+  @Override
+  public MetadataEntity toMetadataEntity() throws UnsupportedOperationException {
+    return MetadataEntity.ofNamespace(namespace).append(MetadataEntity.STREAM, stream);
   }
 
   @Override