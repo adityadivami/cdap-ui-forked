prafullkotecha/cdap

Send preview status changes and preview data from Preview Runner to Preview Manager through TMS.

cdap-app-templates/cdap-etl/cdap-data-pipeline/src/test/java/io/cdap/cdap/datapipeline/preview/PreviewDataPipelineTest.java
@@ -403,14 +403,19 @@ public class PreviewDataPipelineTest extends HydratorTestBase {
   }
 
   private void checkPreviewStore(PreviewRunner previewRunner, ApplicationId previewId, String tracerName,
-                                 int numExpectedRecords) throws NotFoundException {
-    Map<String, List<JsonElement>> result = previewRunner.getData(previewId, tracerName);
-    List<JsonElement> data = result.get(DATA_TRACER_PROPERTY);
-    if (data == null) {
-      Assert.assertEquals(numExpectedRecords, 0);
-    } else {
-      Assert.assertEquals(numExpectedRecords, data.size());
-    }
+                                 int numExpectedRecords) throws Exception {
+    Tasks.waitFor(numExpectedRecords, new Callable<Integer>() {
+      @Override
+      public Integer call() throws Exception {
+        Map<String, List<JsonElement>> result = previewRunner.getData(previewId, tracerName);
+        List<JsonElement> data = result.get(DATA_TRACER_PROPERTY);
+        if (data == null) {
+          return 0;
+        } else {
+          return data.size();
+        }
+      }
+    }, 60, TimeUnit.SECONDS);
   }
 
   private void validateMetric(long expected, ApplicationId previewId,

adityadivami/cdap-ui-forked

cdap-app-templates/cdap-etl/cdap-data-pipeline/src/test/java/io/cdap/cdap/datapipeline/preview/PreviewDataPipelineTest.java
@@ -31,7 +31,6 @@ import io.cdap.cdap.api.metrics.MetricDataQuery;
 import io.cdap.cdap.api.metrics.MetricTimeSeries;
 import io.cdap.cdap.app.preview.PreviewManager;
 import io.cdap.cdap.app.preview.PreviewStatus;
-import io.cdap.cdap.common.NotFoundException;
 import io.cdap.cdap.common.conf.Constants;
 import io.cdap.cdap.common.utils.Tasks;
 import io.cdap.cdap.datapipeline.DataPipelineApp;
@@ -399,14 +398,19 @@ public class PreviewDataPipelineTest extends HydratorTestBase {
   }
 
   private void checkPreviewStore(PreviewManager previewManager, ApplicationId previewId, String tracerName,
-                                 int numExpectedRecords) throws NotFoundException {
-    Map<String, List<JsonElement>> result = previewManager.getData(previewId, tracerName);
-    List<JsonElement> data = result.get(DATA_TRACER_PROPERTY);
-    if (data == null) {
-      Assert.assertEquals(numExpectedRecords, 0);
-    } else {
-      Assert.assertEquals(numExpectedRecords, data.size());
-    }
+                                 int numExpectedRecords) throws Exception {
+    Tasks.waitFor(numExpectedRecords, new Callable<Integer>() {
+      @Override
+      public Integer call() throws Exception {
+        Map<String, List<JsonElement>> result = previewManager.getData(previewId, tracerName);
+        List<JsonElement> data = result.get(DATA_TRACER_PROPERTY);
+        if (data == null) {
+          return 0;
+        } else {
+          return data.size();
+        }
+      }
+    }, 60, TimeUnit.SECONDS);
   }
 
   private void validateMetric(long expected, ApplicationId previewId,