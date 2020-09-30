hensilva/cdap

[CDAP-17299] Add metrics for pipeline and preview run time

cdap-app-fabric/src/main/java/io/cdap/cdap/internal/app/preview/PreviewDataSubscriberService.java
@@ -67,6 +67,7 @@ public class PreviewDataSubscriberService extends AbstractMessagingSubscriberSer
   private final int maxRetriesOnError;
   private int errorCount = 0;
   private String erroredMessageId = null;
+  private MetricsCollectionService metricsCollectionService;
 
   /**
    * Constructor.
@@ -74,7 +75,9 @@ public class PreviewDataSubscriberService extends AbstractMessagingSubscriberSer
   @Inject
   PreviewDataSubscriberService(CConfiguration cConf,
                                @Named(PreviewConfigModule.GLOBAL_TMS) MessagingService messagingService,
-                               MetricsCollectionService metricsCollectionService, PreviewStore previewStore,
+                               @Named(PreviewConfigModule.GLOBAL_METRICS) MetricsCollectionService
+                                 metricsCollectionService,
+                               PreviewStore previewStore,
                                TransactionRunner transactionRunner) {
     super(
       NamespaceId.SYSTEM.topic(cConf.get(Constants.Preview.MESSAGING_TOPIC)),
@@ -94,6 +97,7 @@ public class PreviewDataSubscriberService extends AbstractMessagingSubscriberSer
     this.previewStore = previewStore;
     this.transactionRunner = transactionRunner;
     this.maxRetriesOnError = cConf.getInt(Constants.Metadata.MESSAGING_RETRIES_ON_CONFLICT);
+    this.metricsCollectionService = metricsCollectionService;
   }
 
   @Override
@@ -174,6 +178,20 @@ public class PreviewDataSubscriberService extends AbstractMessagingSubscriberSer
   }
 
   /**
+   * Emit the preview run time metric.
+   */
+  private void emitPreviewRunTimeMetric(PreviewStatus previewStatus, ApplicationId applicationId) {
+    long runTime = (previewStatus.getEndTime() - previewStatus.getStartTime()) / 1000;
+    Map<String, String> tags = ImmutableMap.<String, String>builder()
+      .put(Constants.Metrics.Tag.NAMESPACE, NamespaceId.SYSTEM.getNamespace())
+      .put(Constants.Metrics.Tag.APP, applicationId.getApplication())
+      .put(Constants.Metrics.Tag.COMPONENT, Constants.Service.PREVIEW_HTTP)
+      .build();
+    metricsCollectionService.getContext(tags).gauge(Constants.Metrics.Program.PROGRAM_RUN_TIME_SECONDS,
+                                                    runTime);
+  }
+
+  /**
    * The {@link PreviewMessageProcessor} for processing preview data.
    */
   private final class PreviewDataProcessor implements PreviewMessageProcessor {
@@ -224,6 +242,9 @@ public class PreviewDataSubscriberService extends AbstractMessagingSubscriberSer
         return;
       }
       previewStore.setPreviewStatus(applicationId, payload);
+      if (payload.getStatus().isEndState()) {
+        emitPreviewRunTimeMetric(payload, applicationId);
+      }
     }
   }
 

cdap-app-fabric/src/main/java/io/cdap/cdap/internal/app/services/ProgramNotificationSubscriberService.java
@@ -22,6 +22,7 @@ import com.google.gson.GsonBuilder;
 import com.google.gson.JsonSyntaxException;
 import com.google.gson.reflect.TypeToken;
 import com.google.inject.Inject;
+import com.sun.xml.internal.xsom.impl.Const;
 import io.cdap.cdap.api.metrics.MetricsCollectionService;
 import io.cdap.cdap.api.schedule.SchedulableProgramType;
 import io.cdap.cdap.api.workflow.ScheduleProgramInfo;
@@ -409,6 +410,17 @@ public class ProgramNotificationSubscriberService extends AbstractNotificationSu
 
       getEmitMetricsRunnable(programRunId, recordedRunRecord,
                              STATUS_METRICS_NAME.get(programRunStatus)).ifPresent(runnables::add);
+
+      // emit provisioning time metric
+      long runTime = endTimeSecs - RunIds.getTime(programRunId.getRun(), TimeUnit.SECONDS);
+      ProgramOptions programOptions = ProgramOptions.fromNotification(notification, GSON);
+      Map<String, String> arguments = recordedRunRecord.getSystemArgs();
+      SystemArguments
+        .getProfileIdFromArgs(programRunId.getNamespaceId(), arguments)
+        .ifPresent(profileId -> emitRunTimeMetric(programRunId, profileId,
+                                                  arguments.get(SystemArguments.PROFILE_PROVISIONER),
+                                                  programOptions, runTime));
+
       runnables.add(() -> {
         programCompletionNotifiers.forEach(notifier -> notifier.onProgramCompleted(programRunId,
                                                                                    recordedRunRecord.getStatus()));
@@ -670,6 +682,28 @@ public class ProgramNotificationSubscriberService extends AbstractNotificationSu
     metricsCollectionService.getContext(tags).gauge(Constants.Metrics.Program.PROGRAM_PROVISIONING_DELAY_SECONDS,
                                                     provisioningTime);
   }
+
+  /**
+   * Emit the pipeline run time metric. The tags are constructed with the program run id,
+   * profile id, provisioner name and program options.
+   */
+  private void emitRunTimeMetric(ProgramRunId programRunId, ProfileId profileId, String provisioner,
+                                          ProgramOptions programOptions, long runTime) {
+    Map<String, String> tags = ImmutableMap.<String, String>builder()
+      .put(Constants.Metrics.Tag.PROFILE_SCOPE, profileId.getScope().name())
+      .put(Constants.Metrics.Tag.PROFILE, profileId.getProfile())
+      .put(Constants.Metrics.Tag.NAMESPACE, programRunId.getNamespace())
+      .put(Constants.Metrics.Tag.PROGRAM_TYPE, programRunId.getType().getPrettyName())
+      .put(Constants.Metrics.Tag.APP, programRunId.getApplication())
+      .put(Constants.Metrics.Tag.PROGRAM, programRunId.getProgram())
+      .put(Constants.Metrics.Tag.PROVISIONER, provisioner)
+      .put(Constants.Metrics.Tag.RUN_ID, programRunId.getRun())
+      .put(Constants.Metrics.Tag.COMPONENT, Constants.Service.APP_FABRIC_HTTP)
+      .build();
+    metricsCollectionService.getContext(tags).gauge(Constants.Metrics.Program.PROGRAM_RUN_TIME_SECONDS,
+                                                    runTime);
+  }
+
 
   /**
    * Returns an instance of {@link AppMetadataStore}.

adityadivami/cdap-ui-forked

cdap-app-fabric/src/main/java/io/cdap/cdap/internal/app/preview/PreviewDataSubscriberService.java
@@ -67,6 +67,7 @@ public class PreviewDataSubscriberService extends AbstractMessagingSubscriberSer
   private final int maxRetriesOnError;
   private int errorCount = 0;
   private String erroredMessageId = null;
+  private MetricsCollectionService metricsCollectionService;
 
   /**
    * Constructor.
@@ -74,7 +75,9 @@ public class PreviewDataSubscriberService extends AbstractMessagingSubscriberSer
   @Inject
   PreviewDataSubscriberService(CConfiguration cConf,
                                @Named(PreviewConfigModule.GLOBAL_TMS) MessagingService messagingService,
-                               MetricsCollectionService metricsCollectionService, PreviewStore previewStore,
+                               @Named(PreviewConfigModule.GLOBAL_METRICS) MetricsCollectionService
+                                 metricsCollectionService,
+                               PreviewStore previewStore,
                                TransactionRunner transactionRunner) {
     super(
       NamespaceId.SYSTEM.topic(cConf.get(Constants.Preview.MESSAGING_TOPIC)),
@@ -94,6 +97,7 @@ public class PreviewDataSubscriberService extends AbstractMessagingSubscriberSer
     this.previewStore = previewStore;
     this.transactionRunner = transactionRunner;
     this.maxRetriesOnError = cConf.getInt(Constants.Metadata.MESSAGING_RETRIES_ON_CONFLICT);
+    this.metricsCollectionService = metricsCollectionService;
   }
 
   @Override
@@ -174,6 +178,21 @@ public class PreviewDataSubscriberService extends AbstractMessagingSubscriberSer
   }
 
   /**
+   * Emit the preview run time metric.
+   */
+  private void emitRunTimeMetric(PreviewStatus previewStatus, ApplicationId applicationId) {
+    long runTime = (previewStatus.getEndTime() - previewStatus.getStartTime()) / 1000;
+    Map<String, String> tags = ImmutableMap.<String, String>builder()
+      .put(Constants.Metrics.Tag.NAMESPACE, NamespaceId.SYSTEM.getNamespace())
+      .put(Constants.Metrics.Tag.APP, applicationId.getApplication())
+      .put(Constants.Metrics.Tag.COMPONENT, Constants.Service.PREVIEW_HTTP)
+      .put(Constants.Metrics.Tag.STATUS, previewStatus.getStatus().name())
+      .build();
+    metricsCollectionService.getContext(tags).gauge(Constants.Metrics.Program.PROGRAM_RUN_TIME_SECONDS,
+                                                    runTime);
+  }
+
+  /**
    * The {@link PreviewMessageProcessor} for processing preview data.
    */
   private final class PreviewDataProcessor implements PreviewMessageProcessor {
@@ -224,6 +243,9 @@ public class PreviewDataSubscriberService extends AbstractMessagingSubscriberSer
         return;
       }
       previewStore.setPreviewStatus(applicationId, payload);
+      if (payload.getStatus().isEndState()) {
+        emitRunTimeMetric(payload, applicationId);
+      }
     }
   }
 

cdap-app-fabric/src/main/java/io/cdap/cdap/internal/app/services/ProgramNotificationSubscriberService.java
@@ -409,6 +409,13 @@ public class ProgramNotificationSubscriberService extends AbstractNotificationSu
 
       getEmitMetricsRunnable(programRunId, recordedRunRecord,
                              STATUS_METRICS_NAME.get(programRunStatus)).ifPresent(runnables::add);
+
+      // emit provisioning time metric
+      long runTime = endTimeSecs - RunIds.getTime(programRunId.getRun(), TimeUnit.SECONDS);
+      SystemArguments
+        .getProfileIdFromArgs(programRunId.getNamespaceId(), recordedRunRecord.getSystemArgs())
+        .ifPresent(profileId -> emitRunTimeMetric(programRunId, programRunStatus, runTime));
+
       runnables.add(() -> {
         programCompletionNotifiers.forEach(notifier -> notifier.onProgramCompleted(programRunId,
                                                                                    recordedRunRecord.getStatus()));
@@ -670,6 +677,22 @@ public class ProgramNotificationSubscriberService extends AbstractNotificationSu
     metricsCollectionService.getContext(tags).gauge(Constants.Metrics.Program.PROGRAM_PROVISIONING_DELAY_SECONDS,
                                                     provisioningTime);
   }
+
+  /**
+   * Emit the pipeline run time metric. The tags are constructed with the program run id and program run status.
+   */
+  private void emitRunTimeMetric(ProgramRunId programRunId, ProgramRunStatus programRunStatus,
+                                 long runTime) {
+    Map<String, String> tags = ImmutableMap.<String, String>builder()
+      .put(Constants.Metrics.Tag.NAMESPACE, programRunId.getNamespace())
+      .put(Constants.Metrics.Tag.APP, programRunId.getApplication())
+      .put(Constants.Metrics.Tag.COMPONENT, Constants.Service.APP_FABRIC_HTTP)
+      .put(Constants.Metrics.Tag.STATUS, programRunStatus.name())
+      .build();
+    metricsCollectionService.getContext(tags).gauge(Constants.Metrics.Program.PROGRAM_RUN_TIME_SECONDS,
+                                                    runTime);
+  }
+
 
   /**
    * Returns an instance of {@link AppMetadataStore}.