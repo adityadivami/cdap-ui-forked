029317d066a3c0760e8ae96892575172465f0eeb
aditya <aditya@divami.com>
2022-07-06 10:14:56 +0530

    called connectors and category connections apis, failing

--- app/cdap/components/Wrangler/Home/index.tsx
+++ app/cdap/components/Wrangler/Home/index.tsx
@@ -1,6 +1,33 @@
+import { getCategorizedConnections } from 'components/Connections/Browser/SidePanel/apiHelpers';
+import { fetchConnectors } from 'components/Connections/Create/reducer';
 import * as React from 'react';
+import { useParams } from 'react-router';
 
 const HomeComponent: React.FC = () => {
+  const params = useParams();
+
+  const [selectedConnection, setSelectedConnection] = React.useState(
+    (params as any).connectionid || null
+  );
+
+  React.useEffect(() => {
+    if ((params as any).connectionid !== selectedConnection) {
+      setSelectedConnection((params as any).connectionid);
+    }
+  }, [params]);
+
+  const apis = async () => {
+    const categorizedConnections = await getCategorizedConnections();
+    const connectorTypes = await fetchConnectors();
+
+    console.log(categorizedConnections, 'getCategorizedConnections');
+    console.log(connectorTypes, 'fetchConnectors');
+  };
+
+  React.useEffect(() => {
+    apis();
+  }, []);
+
   return <>Home Component</>;
 };
 
