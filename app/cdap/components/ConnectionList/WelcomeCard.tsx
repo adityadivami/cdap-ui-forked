fddf22b48f914a30f32c5375c43d7367b4248013
Siva Rajana <siva@divami.com>
2022-07-10 19:07:57 +0530

    welcome card's theme provider removed

--- app/cdap/components/ConnectionList/WelcomeCard.tsx
+++ app/cdap/components/ConnectionList/WelcomeCard.tsx
@@ -1,38 +1,36 @@
-import React from "react";
-import { GetIcon } from "./IconStore";
-import { Box, styled, Typography, makeStyles } from "@material-ui/core";
+import React from 'react';
+import { GetIcon } from './IconStore';
+import { Box, styled, Typography, makeStyles } from '@material-ui/core';
 
 const useStyles = makeStyles(() => ({
   welcomeText: {
-    fontSize: "24px",
+    fontSize: '24px',
     fontWeight: 500,
-    height: "36px",
+    height: '36px',
   },
 }));
 
 const WelcomeCardContainer = styled(Box)({
-  display: "flex",
-  paddingTop: "18px",
+  display: 'flex',
+  paddingTop: '18px',
 });
 
 const CustomBox = styled(Box)({
-  display: "flex",
-  flexDirection: "column",
-  justifyContent: "flex-end",
-  marginLeft: "36px",
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'flex-end',
+  marginLeft: '36px',
 });
 
 const WelcomeCard = () => {
   const classes = useStyles();
-  const welcomeIcon = GetIcon("welcomeIcon");
+  const welcomeIcon = GetIcon('welcomeIcon');
   return (
     <WelcomeCardContainer>
       <Box>{welcomeIcon}</Box>
       <CustomBox>
         <Typography className={classes.welcomeText}>Hi David</Typography>
-        <Typography className={classes.welcomeText}>
-          Welcome to Wrangler
-        </Typography>
+        <Typography className={classes.welcomeText}>Welcome to Wrangler</Typography>
       </CustomBox>
     </WelcomeCardContainer>
   );
