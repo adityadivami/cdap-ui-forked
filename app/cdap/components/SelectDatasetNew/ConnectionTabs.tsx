ffc7bea8562dd36178f589be3bef3c2a7c938f5f
Siva Rajana <siva@divami.com>
2022-07-28 18:58:23 +0530

    tabs without styling

---
+++ app/cdap/components/SelectDatasetNew/ConnectionTabs.tsx
@@ -0,0 +1,85 @@
+import { styled, Tooltip, Typography } from '@material-ui/core';
+import Box from '@material-ui/core/Box';
+import Tab from '@material-ui/core/Tab';
+import Tabs from '@material-ui/core/Tabs';
+import * as React from 'react';
+import { useStyles } from './styles';
+import CustomTooltip from './CustomTooltip';
+
+const ConnectionTab = styled(Tab)({
+  minWidth: '161px',
+  padding: '15px 0px 15px 32px',
+  textTransform: 'none',
+  color: 'black',
+  fontSize: '16px',
+  minHeight: '53px !important',
+  '& .MuiTab-labelIcon': { minHeight: '54px !important' },
+  '& .MuiTab-wrapper': {
+    fontSize: '16px',
+    fontWeight: '400',
+    display: 'flex',
+    justifyContent: 'flex-start',
+    gap: '9.41px',
+    flexDirection: 'row',
+    zIndex: 3,
+    whiteSpace: 'nowrap',
+  },
+  '&.MuiTab-root': {
+    width: '252px',
+  },
+  '&.MuiTab-labelIcon .MuiTab-wrapper > *:first-child': {
+    marginBottom: '0px',
+  },
+});
+
+const ConnectionsTabs = ({ tabsData, handleChange, value, index }) => {
+  const classes = useStyles();
+
+  return (
+    <>
+      {tabsData.showTabs && (
+        <Box className={classes.boxStyles}>
+          <Tabs
+            value={value}
+            orientation="vertical"
+            variant="scrollable"
+            TabIndicatorProps={{
+              className: classes.tabIndicatorStyles,
+            }}
+          >
+            {tabsData.data.map((connectorType, connectorTypeIndex) => (
+              <ConnectionTab
+                onClick={() => {
+                  handleChange(connectorType, index);
+                }}
+                label={<TabLabel label={connectorType.name} count={connectorType.count} />}
+                value={connectorType.name}
+                icon={<Box className={classes.iconBoxStyles}>{connectorType.SVG}</Box>}
+                disableTouchRipple
+                key={`${connectorType.name}=${connectorTypeIndex}`}
+                id={connectorType.name}
+              />
+            ))}
+          </Tabs>
+        </Box>
+      )}
+    </>
+  );
+};
+
+const TabLabel = ({ label, count }: { label: string; count: number }) => {
+  const classes = useStyles();
+
+  return (
+    <CustomTooltip title={label.length > 16 ? label : ''} arrow>
+      <Box className={classes.labelsContainer}>
+        <Typography variant="body1" className={classes.labelStyles}>
+          {label}
+        </Typography>
+        <Typography variant="body1" className={classes.labelStyles}>{`(${count})`}</Typography>
+      </Box>
+    </CustomTooltip>
+  );
+};
+
+export default ConnectionsTabs;
