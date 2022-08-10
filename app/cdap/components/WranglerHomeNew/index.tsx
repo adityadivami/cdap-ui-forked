e6908fc08f25092e5ae02c8a6ef832563130af17
smriti <smriti@Smritis-MacBook-Air.local>
2022-08-10 11:32:27 +0530

    API Integrating for Ongoing Data Exploration

--- app/cdap/components/WranglerHomeNew/index.tsx
+++ app/cdap/components/WranglerHomeNew/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import OngoingDataExploration from 'components/OngoingDataExploration';
 import WrangleCard from 'components/WrangleCard';
 import Box from '@material-ui/core/Box';
@@ -7,9 +7,67 @@ import { GradientLine } from './icons/GradientLine';
 import { HeaderImage } from './icons/HeaderImage';
 import WrangleHomeTitle from 'components/WrangleHomeTitle';
 import { useStyles } from './styles';
+import MyDataPrepApi from 'api/dataprep';
 
 const WranglerHomeNew = () => {
   const classes = useStyles();
+  const [ongoingExpData, setOngoingExpData] = useState<any>([]);
+
+  const getOngoingData = () => {
+    MyDataPrepApi.getWorkspaceList({
+      context: 'default',
+    }).subscribe((res) => {
+      console.log(res);
+      res.values.forEach((item) => {
+        const params = {
+          context: 'default',
+          workspaceId: item.workspaceId,
+        };
+        const requestBody = {
+          directives: item.directives,
+          limit: 1000,
+          insights: {
+            name: item.name,
+            workspaceName: item.workspaceName,
+            path: item?.sampleSpec?.path,
+            visualization: {},
+          },
+        };
+        MyDataPrepApi.execute(params, requestBody).subscribe((response) => {
+          console.log(`Response for ${params.workspaceId}`, response);
+          let dataQuality = 0;
+          response.headers.forEach((head) => {
+            const general = response.summary.statistics[head].general;
+            const { empty: empty = 0, 'non-null': nonEmpty = 100 } = general;
+            const nonNull = Math.floor((nonEmpty - empty) * 10) / 10;
+            dataQuality = dataQuality + nonNull;
+          });
+
+          const totalDataQuality = dataQuality / response.headers.length;
+
+          setOngoingExpData((current) => [
+            ...current,
+            {
+              connectionName:
+                item?.sampleSpec?.connectionName === undefined
+                  ? 'Upload'
+                  : item?.sampleSpec?.connectionName,
+              workspaceName: item.workspaceName,
+              recipeSteps: item.directives.length,
+              dataQuality: totalDataQuality,
+            },
+          ]);
+        });
+      });
+    });
+  };
+
+  useEffect(() => {
+    getOngoingData();
+  }, []);
+  useEffect(() => {
+    console.log('ongoingExpData', ongoingExpData);
+  }, [ongoingExpData]);
   return (
     <Box className={classes.wrangleHomeWrapper}>
       <Box className={classes.subHeader}>
