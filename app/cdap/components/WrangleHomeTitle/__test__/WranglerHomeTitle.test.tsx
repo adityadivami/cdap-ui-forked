bf9d5c4b614eb97acd5c58961f39f28554183cb6
Abhilash <abhilash@Abhilash.local>
2022-08-19 20:29:10 +0530

    feat: unit test

--- app/cdap/components/WrangleHomeTitle/__test__/WranglerHomeTitle.test.tsx
+++ app/cdap/components/WrangleHomeTitle/__test__/WranglerHomeTitle.test.tsx
@@ -2,9 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import WranglerHomeTitle from '../index';
 
-test('renders Wrangler-Home-New ', () => {
+test('renders Wrangler-Home-New component with correct tiltle', () => {
   render(<WranglerHomeTitle title="Hello-World" />);
   const ele = screen.getByTestId(/wrangler-home-title-text/i);
-  // expect(ele.innerText).toBe('Hello-World');
   expect(ele).toHaveTextContent('Hello-World');
 });
