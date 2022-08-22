97957d5941872d390a7b4cb745e6055c1dafb0c4
Gayathri <Gayathri@gayathris-MacBook-Air.local>
2022-08-22 17:11:05 +0530

    FooterPanelnew

--- app/cdap/components/FooterPanel/index.tsx
+++ app/cdap/components/FooterPanel/index.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import { Box } from '@material-ui/core';
 import './index.scss';
-import { Svg, Svg1 } from './images';
+import { Svg, Svg1, Svg2 } from './images';
 const Footer = () => {
   return (
-    <Box className="cont">
-      <Box className="cont1">{Svg}</Box>
-      <Box className="cont2">
-        <p className="paraele"> Current data - 1000 rows and 30 columns</p>
+    <Box className="main">
+      <Box className="cont">
+        <Box className="cont1">{Svg}</Box>
+        <Box className="cont2">
+          <p className="paraele"> Current data - 1000 rows and 30 columns</p>
+        </Box>
+        <Box className="cont3">
+          {Svg1}
+          <span className="spanele"> 100%</span>
+          {Svg2}
+        </Box>
+        <p className="cont4">Directives</p>
+        <Box className="cont5">
+          <p> Recipe Steps</p>
+          <p className="spanele1"> 10</p>
+        </Box>
       </Box>
     </Box>
   );
