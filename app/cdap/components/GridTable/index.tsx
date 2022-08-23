7b100e7fa8014afa45379944a92d9317a5901439
Richa <richa@divami.com>
2022-08-23 19:51:53 +0530

    Wrangler grid workspace console

--- app/cdap/components/GridTable/index.tsx
+++ app/cdap/components/GridTable/index.tsx
@@ -1,5 +1,5 @@
 import { Box, Table, TableBody, TableContainer, TableHead, TableRow } from '@material-ui/core';
-import { default as React, useEffect } from 'react';
+import { default as React, useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import BreadCumb from './components/Breadcrumb';
 import { GridHeaderCell } from './components/GridHeaderCell';
@@ -7,12 +7,68 @@ import { GridKPICell } from './components/GridKPICell';
 import { GridTextCell } from './components/GridTextCell';
 import mockJSON from './mock/apiMock';
 import metricsJSON from './mock/metrics';
+import MyDataPrepApi from 'api/dataprep';
 
 const GridTable = () => {
   const { datasetName } = useParams() as any;
 
   const [headersNamesList, setHeadersNamesList] = React.useState([]);
   const [rowsDataList, setRowsDataList] = React.useState([]);
+
+  const [ongoingExpDatas, setOngoingExpDatas] = useState<any>([]);
+  const [finalArray, setFinalArray] = useState([]);
+
+  const getOngoingData = () => {
+    MyDataPrepApi.getWorkspaceList({
+      context: 'default',
+    }).subscribe((res) => {
+      res.values.forEach((item) => {
+        const params = {
+          context: 'default',
+          workspaceId: item.workspaceId,
+        };
+        const requestBody = {
+          directives: item.directives,
+          limit: 1000,
+          insights: {
+            name: item.name,
+            workspaceName: item.workspaceName,
+            path: item?.sampleSpec?.path,
+            visualization: {},
+          },
+        };
+        MyDataPrepApi.execute(params, requestBody).subscribe((response) => {
+          console.log('response', response);
+          let dataQuality = 0;
+          response.headers.forEach((head) => {
+            const general = response.summary.statistics[head].general;
+            const { empty: empty = 0, 'non-null': nonEmpty = 100 } = general;
+            const nonNull = Math.floor((nonEmpty - empty) * 10) / 10;
+            dataQuality = dataQuality + nonNull;
+          });
+
+          const totalDataQuality = dataQuality / response.headers.length;
+
+          setOngoingExpDatas((current) => [
+            ...current,
+            {
+              connectionName:
+                item?.sampleSpec?.connectionName === undefined
+                  ? 'Upload'
+                  : item?.sampleSpec?.connectionName,
+              workspaceName: item.workspaceName,
+              recipeSteps: item.directives.length,
+              dataQuality: totalDataQuality,
+            },
+          ]);
+        });
+      });
+    });
+  };
+
+  useEffect(() => {
+    getOngoingData();
+  }, []);
 
   const createHeadersData = (columnNamesList, columnLabelsList, columnTypesList) => {
     return columnNamesList
@@ -49,7 +105,7 @@ const GridTable = () => {
     <>
       <BreadCumb datasetName={datasetName} />
       <TableContainer component={Box}>
-        <Table aria-label="simple table">
+        <Table aria-label="simple table" className="test">
           <TableHead>
             <TableRow>
               {headersNamesList.map((eachHeader) => (
