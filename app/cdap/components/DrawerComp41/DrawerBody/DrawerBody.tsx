1d248c65d0be3c76347e936928b0f3c189104659
gayathriredd <gayathri@divami.com>
2022-08-29 11:32:01 +0530

    component41

--- app/cdap/components/DrawerComp41/DrawerBody/DrawerBody.tsx
+++ app/cdap/components/DrawerComp41/DrawerBody/DrawerBody.tsx
@@ -3,109 +3,105 @@ import { Divider } from '@material-ui/core';
 import { useDrawerCss } from '../styles';
 import { Icon65, Icon74, Icon100, Icon89, Icon96, Icon6 } from '../iconStore';
 
-const DrawerBody = (props) => {
+const DrawerBody = () => {
   const classes = useDrawerCss();
 
   return (
     <React.Fragment>
-      {props.bodyComponent ? (
-        props.bodyComponent()
-      ) : (
-        <div>
-          <div className={classes.flexBetweenBaseLine + ' ' + classes.containerWrapper}>
-            <p className={classes.weight40}> Columns (30)</p>
-            <p className={classes.weight40}> Data Quality</p>
-          </div>
-          <Divider />
-
-          <div className={classes.flexBetweenBaseLine + ' ' + classes.containerWrapper}>
-            <div className={classes.flexOnly}>
-              <div>
-                <p className={classes.weight400 + ' ' + classes.marginTopp}> Customer Name</p>
-                <p className={classes.weight400}> ABC | String</p>
-              </div>
-            </div>
-            <p className={classes.weight400}> {Icon65}</p>
-          </div>
-          <Divider />
-
-          <div
-            className={
-              classes.flexBetweenBaseLine +
-              ' ' +
-              classes.containerWrapper +
-              ' ' +
-              classes.paddingLeftt +
-              ' ' +
-              classes.boxShadoww
-            }
-          >
-            <div className={classes.flexOnly}>
-              <div className={classes.iconStyles}> {Icon6}</div>
-              <div>
-                <p className={classes.weight400}> Region</p>
-                <p className={classes.weight400}> ABC | String</p>
-              </div>
-            </div>
-            <p className={classes.weight400}> {Icon74}</p>
-          </div>
-          <Divider />
-
-          <div className={classes.flexBetweenBaseLine + ' ' + classes.containerWrapper}>
-            <div className={classes.flexOnly}>
-              <div>
-                <p className={classes.weight400}> Car Model</p>
-                <p className={classes.weight400}> ABC | String</p>
-              </div>
-            </div>
-            <p className={classes.weight400}> {Icon100}</p>
-          </div>
-          <Divider />
-
-          <div className={classes.flexBetweenBaseLine + ' ' + classes.containerWrapper}>
-            <div className={classes.flexOnly}>
-              <div>
-                <p className={classes.weight400}> CC Number</p>
-                <p className={classes.weight400}> 123 | Credit Card</p>
-              </div>
-            </div>
-            <p className={classes.weight400}> {Icon100}</p>
-          </div>
-          <Divider />
-
-          <div className={classes.flexBetweenBaseLine + ' ' + classes.containerWrapper}>
-            <div className={classes.flexOnly}>
-              <div>
-                <p className={classes.weight400}> Purchase Date</p>
-                <p className={classes.weight400}> ABC | Date and Time</p>
-              </div>
-            </div>
-            <p className={classes.weight400}> {Icon100}</p>
-          </div>
-          <Divider />
-
-          <div className={classes.flexBetweenBaseLine + ' ' + classes.containerWrapper}>
-            <div className={classes.flexOnly}>
-              <div>
-                <p className={classes.weight400}> Payment Mode</p>
-                <p className={classes.weight400}> ABC | String</p>
-              </div>
-            </div>
-            <p className={classes.weight400}> {Icon89}</p>
-          </div>
-          <Divider />
-
-          <div className={classes.flexBetweenBaseLine + ' ' + classes.containerWrapper}>
-            <div className={classes.flexOnly}>
-              <div>
-                <p className={classes.weight400}> Order amount</p>
-                <p className={classes.weight400}> 123 | String</p>
-              </div>
-            </div>
-            <p className={classes.weight400}> {Icon96}</p>
-          </div>
+      <div>
+        <div className={classes.flexBetweenBaseLine + ' ' + classes.containerWrapper}>
+          <p className={classes.weight40}> Columns (30)</p>
+          <p className={classes.weight40}> Data Quality</p>
         </div>
-      )}
+        <Divider />
+
+        <div className={classes.flexBetweenBaseLine + ' ' + classes.containerWrapper}>
+          <div className={classes.flexOnly}>
+            <div>
+              <p className={classes.weight400 + ' ' + classes.marginTopp}> Customer Name</p>
+              <p className={classes.weight400}> ABC | String</p>
+            </div>
+          </div>
+          <p className={classes.weight400}> {Icon65}</p>
+        </div>
+        <Divider />
+
+        <div
+          className={
+            classes.flexBetweenBaseLine +
+            ' ' +
+            classes.containerWrapper +
+            ' ' +
+            classes.paddingLeftt +
+            ' ' +
+            classes.boxShadoww
+          }
+        >
+          <div className={classes.flexOnly}>
+            <div className={classes.iconStyles}> {Icon6}</div>
+            <div>
+              <p className={classes.weight400}> Region</p>
+              <p className={classes.weight400}> ABC | String</p>
+            </div>
+          </div>
+          <p className={classes.weight400}> {Icon74}</p>
+        </div>
+        <Divider />
+
+        <div className={classes.flexBetweenBaseLine + ' ' + classes.containerWrapper}>
+          <div className={classes.flexOnly}>
+            <div>
+              <p className={classes.weight400}> Car Model</p>
+              <p className={classes.weight400}> ABC | String</p>
+            </div>
+          </div>
+          <p className={classes.weight400}> {Icon100}</p>
+        </div>
+        <Divider />
+
+        <div className={classes.flexBetweenBaseLine + ' ' + classes.containerWrapper}>
+          <div className={classes.flexOnly}>
+            <div>
+              <p className={classes.weight400}> CC Number</p>
+              <p className={classes.weight400}> 123 | Credit Card</p>
+            </div>
+          </div>
+          <p className={classes.weight400}> {Icon100}</p>
+        </div>
+        <Divider />
+
+        <div className={classes.flexBetweenBaseLine + ' ' + classes.containerWrapper}>
+          <div className={classes.flexOnly}>
+            <div>
+              <p className={classes.weight400}> Purchase Date</p>
+              <p className={classes.weight400}> ABC | Date and Time</p>
+            </div>
+          </div>
+          <p className={classes.weight400}> {Icon100}</p>
+        </div>
+        <Divider />
+
+        <div className={classes.flexBetweenBaseLine + ' ' + classes.containerWrapper}>
+          <div className={classes.flexOnly}>
+            <div>
+              <p className={classes.weight400}> Payment Mode</p>
+              <p className={classes.weight400}> ABC | String</p>
+            </div>
+          </div>
+          <p className={classes.weight400}> {Icon89}</p>
+        </div>
+        <Divider />
+
+        <div className={classes.flexBetweenBaseLine + ' ' + classes.containerWrapper}>
+          <div className={classes.flexOnly}>
+            <div>
+              <p className={classes.weight400}> Order amount</p>
+              <p className={classes.weight400}> 123 | String</p>
+            </div>
+          </div>
+          <p className={classes.weight400}> {Icon96}</p>
+        </div>
+      </div>
     </React.Fragment>
   );
 };
--- app/cdap/components/DrawerComp41/DrawerHeader/DrawerHeader.tsx
+++ app/cdap/components/DrawerComp41/DrawerHeader/DrawerHeader.tsx
@@ -1,27 +1,22 @@
 import React from 'react';
 import { useDrawerCss } from '../styles';
 import { closeOutLine, UnderLine, SearchIcon, Line } from '../iconStore';
-import { IconButton } from '@material-ui/core';
 
 const DrawerHeader = (props) => {
   const classes = useDrawerCss();
   return (
     <React.Fragment>
-      {props.isShowHeader && props.headerComponent ? (
-        props.headerComponent()
-      ) : (
-        <div className={classes.flexBetweenBaseLine + ' ' + classes.paddingDiv}>
-          <div>
-            <h3 className={classes.headerTitle}>Column View</h3>
-            {UnderLine}
-          </div>
-          <div className={classes.flexOnly}>
-            <div className={classes.paddingDivv}> {SearchIcon}</div>
-            <div className={classes.paddingDivv}> {Line} </div>
-            <div onClick={props.toggleDrawer}>{closeOutLine()}</div>
-          </div>
+      <div className={classes.flexBetweenBaseLine + ' ' + classes.paddingDiv}>
+        <div>
+          <h3 className={classes.headerTitle}>Column View</h3>
+          {UnderLine}
         </div>
-      )}
+        <div className={classes.flexOnly}>
+          <div className={classes.paddingDivv}> {SearchIcon}</div>
+          <div className={classes.paddingDivv}> {Line} </div>
+          <div onClick={props.toggleDrawer}>{closeOutLine()}</div>
+        </div>
+      </div>
     </React.Fragment>
   );
 };
