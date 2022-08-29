d565e0f6fa65bae0f8704bb72b274b6471b75a06
gayathriredd <gayathri@divami.com>
2022-08-29 11:44:57 +0530

    component37

--- app/cdap/components/DrawerComp/DrawerHeader/DrawerHeader.tsx
+++ app/cdap/components/DrawerComp/DrawerHeader/DrawerHeader.tsx
@@ -6,17 +6,13 @@ const DrawerHeader = (props) => {
   const classes = useDrawerCss();
   return (
     <React.Fragment>
-      {props.isShowHeader && props.headerComponent ? (
-        props.headerComponent()
-      ) : (
-        <div className={classes.flexBetweenBaseLine + ' ' + classes.paddingDiv}>
-          <div>
-            <h1 className={classes.headerTitle}>Add Transformation Step</h1>
-            {UnderLine}
-          </div>
-          <div onClick={props.toggleDrawer}>{closeOutLine()}</div>
+      <div className={classes.flexBetweenBaseLine + ' ' + classes.paddingDiv}>
+        <div>
+          <h1 className={classes.headerTitle}>Add Transformation Step</h1>
+          {UnderLine}
         </div>
-      )}
+        <div onClick={props.toggleDrawer}>{closeOutLine()}</div>
+      </div>
     </React.Fragment>
   );
 };
