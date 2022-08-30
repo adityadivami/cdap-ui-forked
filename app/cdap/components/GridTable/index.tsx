f4c5d1e9586a01b341dd7d44238dbc13a4c01c10
tharunpola <tharun@divami.com>
2022-08-30 22:06:39 +0530

    Nested menu component

--- app/cdap/components/GridTable/index.tsx
+++ app/cdap/components/GridTable/index.tsx
@@ -32,6 +32,7 @@ import Box from '@material-ui/core/Box';
 import { useStyles } from './styles';
 import { flatMap } from 'rxjs/operators';
 import { forkJoin } from 'rxjs/observable/forkJoin';
+import ToolBarList from './components/AaToolbar';
 
 const GridTable = () => {
   const { wid } = useParams() as any;
@@ -232,6 +233,7 @@ const GridTable = () => {
   return (
     <Box className={classes.wrapper}>
       <BreadCrumb datasetName={wid} />
+      <ToolBarList />
       <Table aria-label="simple table" className="test">
         <TableHead>
           <TableRow>
