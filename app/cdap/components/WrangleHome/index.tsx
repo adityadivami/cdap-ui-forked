56480cca7b12809b85cd6f4738d093af2f8fa24a
smritiDivami <smriti@divami.com>
2022-09-01 13:49:00 +0530

    view all bug fix

--- app/cdap/components/WrangleHome/index.tsx
+++ app/cdap/components/WrangleHome/index.tsx
@@ -1,4 +1,19 @@
 /*
+ *  Copyright © 2022 Cask Data, Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ *  use this file except in compliance with the License. You may obtain a copy of
+ *  the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ *  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ *  License for the specific language governing permissions and limitations under
+ *  the License.
+ */
+/*
  * Copyright © 2022 Cask Data, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
@@ -43,12 +58,12 @@ const WranglerHome = () => {
       <Box>
         <Box className={classes.headerTitle}>
           <WrangleHomeTitle title="Start data exploration" />
-          <Box className={classes.viewMore}>View More</Box>
+          <Box className={classes.viewMore}>View all</Box>
         </Box>
         <WrangleCard />
         <Box className={classes.headerTitle}>
           <WrangleHomeTitle title="Continue ongoing data explorations, pick up where you left off" />
-          <Box className={classes.viewMore}>View More</Box>
+          <Box className={classes.viewMore}>View all</Box>
         </Box>
         <OngoingDataExploration />
         <If condition={loading}>
