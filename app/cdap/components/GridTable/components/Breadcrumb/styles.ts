e0d693ffd9f6e859dcf77b16fa1c6dac998ec9e3
Gayathri <Gayathri@gayathris-MacBook-Air.local>
2022-09-06 11:59:08 +0530

    BugFixes

--- app/cdap/components/GridTable/components/Breadcrumb/styles.ts
+++ app/cdap/components/GridTable/components/Breadcrumb/styles.ts
@@ -26,7 +26,7 @@ export const useStyles = makeStyles({
     marginRight: '30px',
     marginLeft: '34px',
   },
-  breadcrumblabel: {
+  breadCrumbLabel: {
     color: blue[500],
     fontSize: '14px',
     fontWeight: 400,
