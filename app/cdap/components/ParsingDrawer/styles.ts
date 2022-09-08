e501fa2cb7e56585e1a09e925cc651c7857d0d7b
Manisha <maneesha@divami.com>
2022-09-08 22:58:11 +0530

    Add distribution graph

--- app/cdap/components/ParsingDrawer/styles.ts
+++ app/cdap/components/ParsingDrawer/styles.ts
@@ -24,7 +24,6 @@ export const useStyles = makeStyles(() => {
     headingStyles: {
       display: 'flex',
       flexDirection: 'column',
-      alignItems: 'flex-start',
     },
     pointerStyles: {
       cursor: 'pointer',
