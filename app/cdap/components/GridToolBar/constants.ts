c263cbf217b79a989af78abbd5c2e0f969263161
GayathriRedd <gayathri@divami.com>
2022-09-12 22:07:20 +0530

    searchFunctionality

---
+++ app/cdap/components/GridToolBar/constants.ts
@@ -0,0 +1,34 @@
+export const searchItems = [
+  {
+    option: 'Remove missing ang null',
+    val: 'remove all missing and null values in the column',
+  },
+  {
+    option: 'Remove missing',
+    val: 'remove missing and column',
+  },
+  {
+    option: 'Remove missing null',
+    val: 'remove all null values in the column',
+  },
+  {
+    option: 'Remove letters',
+    val: 'remove letters from the string',
+  },
+  {
+    option: 'Remove numbers',
+    val: 'remove all numerical characters from the string',
+  },
+  {
+    option: 'Remove special characters',
+    val: 'remove all numerical characters from the string',
+  },
+  {
+    option: 'delete columns',
+    val: 'delete the selected columns',
+  },
+  {
+    option: 'masking data',
+    val: 'masking data based on specific criteria',
+  },
+];
--- app/cdap/components/GridToolBar/index.tsx
+++ app/cdap/components/GridToolBar/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Box from '@material-ui/core/Box';
 import { useCss } from './styles';
 import { IconButton } from '@material-ui/core';
+import { searchItems } from './constants';
 import {
   Undo,
   Redo,
