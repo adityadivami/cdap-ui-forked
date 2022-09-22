bec82e50d960aceac6a1de9f7a44b2e2b1374fb5
sivaRajanaDivami <siva@divami.com>
2022-09-22 17:11:32 +0530

    first time parsing panel

--- app/cdap/components/GridTable/index.tsx
+++ app/cdap/components/GridTable/index.tsx
@@ -2,7 +2,7 @@
  * Copyright © 2022 Cask Data, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
+ * use this file except in compliance with the License. You may obtain `a` copy of
  * the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
@@ -190,6 +190,7 @@ export default function GridTable() {
         return;
       } else {
         applyDirectiveAPICall(newDirective);
+        setIsFirstWrangle(false);
       }
     }
   };
