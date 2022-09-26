e4c97e2954469ea3838f1f1fbd299fe8549d06dd
adityadivami <aditya@divami.com>
2022-09-26 12:48:02 +0530

    formatting auto fixes

--- app/cdap/components/ConnectionList/Components/TabLabelCanSample/types.ts
+++ app/cdap/components/ConnectionList/Components/TabLabelCanSample/types.ts
@@ -1,6 +1,6 @@
-import React from "react";
+import React from 'react';
 
-export interface IMessageState{
-    open: boolean;
-    message: string;
+export interface IMessageState {
+  open: boolean;
+  message: string;
 }
