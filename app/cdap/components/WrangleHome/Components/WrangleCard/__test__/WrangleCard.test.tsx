93af7157a6f349e9474f92e9127455acb65b290f
smritiDivami <smriti@divami.com>
2022-09-29 12:36:40 +0530

    Unit Testing Wrangle Home

--- app/cdap/components/WrangleHome/Components/WrangleCard/__test__/WrangleCard.test.tsx
+++ app/cdap/components/WrangleHome/Components/WrangleCard/__test__/WrangleCard.test.tsx
@@ -19,34 +19,35 @@ import { render, screen } from '@testing-library/react';
 import { WrangleCard } from '../index';
 
 test('It renders Wrangler-Card ', () => {
-  jest.mock('components/Connections/Create/reducer', () => {
-    return Promise.resolve([
-      {
-        artifact: { name: 'words', scope: '', version: 'ten' },
-        category: 'hello',
-        classname: 'yolo',
-        description: 'hello',
-        name: 'HeMan',
-        type: 'js',
-      },
-      {
-        artifact: { name: 'words', scope: '', version: 'ten' },
-        category: 'hello',
-        classname: 'yolo',
-        description: 'hello',
-        name: 'BatMan',
-        type: 'js',
-      },
-      {
-        artifact: { name: 'words', scope: '', version: 'ten' },
-        category: 'hello',
-        classname: 'yolo',
-        description: 'hello',
-        name: 'SuperMan',
-        type: 'js',
-      },
-    ]);
-  });
+  // jest.mock('components/Connections/Create/reducer', () => {
+  //   return Promise.resolve([
+  //     {
+  //       artifact: { name: 'words', scope: '', version: 'ten' },
+  //       category: 'hello',
+  //       classname: 'yolo',
+  //       description: 'hello',
+  //       name: 'HeMan',
+  //       type: 'js',
+  //     },
+  //     {
+  //       artifact: { name: 'words', scope: '', version: 'ten' },
+  //       category: 'hello',
+  //       classname: 'yolo',
+  //       description: 'hello',
+  //       name: 'BatMan',
+  //       type: 'js',
+  //     },
+  //     {
+  //       artifact: { name: 'words', scope: '', version: 'ten' },
+  //       category: 'hello',
+  //       classname: 'yolo',
+  //       description: 'hello',
+  //       name: 'SuperMan',
+  //       type: 'js',
+  //     },
+  //   ]);
+  // });
+
   render(<WrangleCard />);
   const ele = screen.getByTestId(/wrangle-card-parent/i);
   expect(ele).toBeInTheDocument();
