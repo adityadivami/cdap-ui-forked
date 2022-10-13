ef9288bfb93605139831ed84cd04eab515d33985
GayathriRedd <gayathri@divami.com>
2022-10-13 12:42:28 +0530

    workspacesopen

--- app/cdap/components/GridTable/components/Breadcrumb/index.tsx
+++ app/cdap/components/GridTable/components/Breadcrumb/index.tsx
@@ -24,6 +24,7 @@ import { DATASOURCES_URL_PARAM, HOME_LABLE, MATCH_SOURCE, WORKSPACE_LIST } from 
 import { icon, icon1 } from './images';
 import IngestViewSchemaDropDown from './KebabMenu';
 import { useStyles } from './styles';
+import OpenWorkspaces from '../openworkspaces'
 
 const BreadCrumb = ({ datasetName, location, setOpenPipeline, setOpenViewSchema }) => {
   const classes = useStyles();
@@ -52,7 +53,11 @@ const BreadCrumb = ({ datasetName, location, setOpenPipeline, setOpenViewSchema 
           </Link>
         )}
         <Typography color="textPrimary">{datasetName}</Typography>
+        <Breadcrumbs>
+        <OpenWorkspaces/>
+        </Breadcrumbs>
       </Breadcrumbs>
+      
       <Breadcrumbs separator=" ">
         <IconButton>{icon}</IconButton>
         <IconButton>{icon1}</IconButton>
