c993d44180885e45ef25b75f81b9dac7c8686d1b
smritiDivami <smriti@divami.com>
2022-10-20 19:03:13 +0530

    Fixed margin

--- app/cdap/components/ParsingDrawer/styles.ts
+++ app/cdap/components/ParsingDrawer/styles.ts
@@ -73,7 +73,7 @@ export const useStyles = makeStyles(() => {
       width: 1,
       height: 28,
       backgroundColor: '#DADCE0',
-      margin: 0.15,
+      margin: '0px 15px',
     },
     checkboxStyles: {
       display: 'flex',
