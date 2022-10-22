16708b4a9f2cbddf1ede1c15faa7b0a34d726995
richadivami <richa@divami.com>
2022-10-22 19:45:23 +0530

    Directive input

---
+++ app/cdap/components/DirectiveInput/Components/AutoComplete/index.tsx
@@ -0,0 +1,271 @@
+/*
+ * Copyright © 2017 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+import React, { useEffect, useState } from 'react';
+import MyDataPrepApi from 'api/dataprep';
+import Fuse from 'fuse.js';
+import uuidV4 from 'uuid/v4';
+import reverse from 'lodash/reverse';
+import Mousetrap from 'mousetrap';
+import NamespaceStore from 'services/NamespaceStore';
+import ee from 'event-emitter';
+import globalEvents from 'services/global-events';
+import { useStyles } from './styles';
+import { Box, Typography } from '@material-ui/core';
+import { IAutoCompleteProps, IOnRowClickValue } from './types';
+
+export default function({
+  setDirectivesList,
+  isDirectiveSelected,
+  columnNamesList,
+  onRowClick,
+  getDirectiveUsage,
+  onColumnSelected,
+  directiveInput,
+}: IAutoCompleteProps) {
+  const [activeResults, setActiveResults] = useState([]);
+  const [input, setInput] = useState<string>('');
+  const [activeSelectionIndex, setActiveSelectionIndex] = useState<number | null>(null);
+  const eventEmitter = ee(ee);
+  const [fuse, setFuse] = useState(null);
+  const classes = useStyles();
+  const getUsage = () => {
+    if (isDirectiveSelected === false) {
+      const namespace = NamespaceStore.getState().selectedNamespace;
+      MyDataPrepApi.getUsage({ context: namespace }).subscribe((res) => {
+        const fuseOptions = {
+          includeScore: true,
+          includeMatches: true,
+          caseSensitive: false,
+          threshold: 0,
+          location: 0,
+          shouldSort: true,
+          distance: 100,
+          minMatchCharLength: 1,
+          maxPatternLength: 32,
+          keys: ['directive'],
+        };
+        setDirectivesList(res.values);
+        setFuse(new Fuse(res.values, fuseOptions));
+      });
+    } else {
+      const fuseOptions = {
+        includeScore: true,
+        includeMatches: true,
+        caseSensitive: false,
+        threshold: 0,
+        shouldSort: true,
+        location: 0,
+        distance: 100,
+        minMatchCharLength: 1,
+        maxPatternLength: 32,
+        keys: ['label'],
+      };
+      setFuse(new Fuse(columnNamesList, fuseOptions));
+    }
+  };
+
+  useEffect(() => {
+    getUsage();
+  }, [isDirectiveSelected]);
+
+  useEffect(() => {
+    eventEmitter.on(globalEvents.DIRECTIVEUPLOAD, getUsage);
+    const directiveInput = document.getElementById('directive-input-search');
+    const mousetrap = new Mousetrap(directiveInput);
+    mousetrap.bind('up', handleUpArrow);
+    mousetrap.bind('down', handleDownArrow);
+    mousetrap.bind('enter', handleEnterKey);
+    mousetrap.bind('tab', handleTabKey);
+
+    // returned function will be called on component unmount
+    return () => {
+      mousetrap.unbind('up');
+      mousetrap.unbind('down');
+      mousetrap.unbind('enter');
+      mousetrap.unbind('tab');
+      eventEmitter.off(globalEvents.DIRECTIVEUPLOAD, getUsage);
+    };
+  });
+  const handleUpArrow = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.preventDefault) {
+      event.preventDefault();
+    }
+    if (activeSelectionIndex === 0) {
+      return;
+    }
+    setActiveSelectionIndex(activeSelectionIndex - 1);
+  };
+
+  const handleDownArrow = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.preventDefault) {
+      event.preventDefault();
+    }
+    if (activeSelectionIndex === activeResults.length - 1) {
+      return;
+    }
+    setActiveSelectionIndex(activeSelectionIndex + 1);
+  };
+
+  const handleEnterKey = () => {
+    if (input.length === 0) {
+      return;
+    }
+    const selectedDirective = activeResults[activeSelectionIndex];
+
+    if (selectedDirective) {
+      handleRowClick(activeResults[activeSelectionIndex]);
+    } else {
+      const eventObject = {
+        target: { value: `${input}` },
+      };
+      onRowClick(eventObject);
+    }
+  };
+
+  const handleTabKey = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (input.length === 0 || input.split(' ').length !== 1) {
+      return;
+    }
+    if (event.preventDefault) {
+      event.preventDefault();
+    }
+    handleEnterKey();
+  };
+
+  useEffect(() => {
+    if (
+      (isDirectiveSelected === true || isDirectiveSelected == undefined) &&
+      isDirectiveSelected !== false
+    ) {
+      const fuseOptions = {
+        includeScore: true,
+        includeMatches: true,
+        caseSensitive: false,
+        threshold: 0,
+        shouldSort: true,
+        location: 0,
+        distance: 100,
+        minMatchCharLength: 1,
+        maxPatternLength: 32,
+        keys: ['label'],
+      };
+      setFuse(new Fuse(columnNamesList, fuseOptions));
+    }
+    searchMatch(directiveInput);
+    setInput(directiveInput);
+  }, [directiveInput]);
+
+  const searchMatch = (query: string) => {
+    let results = [];
+    const input: string = query;
+    const spaceIndex = input.indexOf(' ');
+    if (fuse && input.length > 0) {
+      if (isDirectiveSelected === false) {
+        results = fuse
+          .search(input)
+          .slice(0, 3)
+          .filter((row, index) => {
+            if (spaceIndex === -1) {
+              return true;
+            }
+            return row.score === 0 && index === 0;
+          })
+          .map((row) => {
+            row.uniqueId = uuidV4();
+            return row;
+          });
+        reverse(results);
+      } else {
+        results = fuse.search(input.split(':')[1]).map((row) => {
+          row.uniqueId = uuidV4();
+          return row;
+        });
+        reverse(results);
+      }
+    }
+    setActiveResults(results);
+    setInput(query);
+    setActiveSelectionIndex(results.length - 1);
+    if (isDirectiveSelected === false) {
+      if ((spaceIndex !== -1) === true) {
+        getDirectiveUsage(results, true);
+      } else {
+        getDirectiveUsage(results, false);
+      }
+    }
+  };
+
+  const handleRowClick = (row) => {
+    if (typeof onRowClick !== 'function') {
+      return;
+    }
+    let eventObject = {} as IOnRowClickValue;
+    if (isDirectiveSelected === false) {
+      eventObject = {
+        target: { value: `${row.item.directive}` },
+      };
+      onRowClick(eventObject);
+      getDirectiveUsage([row], true);
+    } else {
+      const splitData = input.split(/(?=[:])|(?<=[:])/g);
+      eventObject = {
+        target: { value: `${splitData[0]}${splitData[1]}${row.item.label}` },
+      };
+      setInput(`${splitData[0]}${splitData[1]}${row.item.label}`);
+      onRowClick(eventObject);
+      onColumnSelected(true);
+    }
+  };
+
+  return (
+    <Box className={classes.listWrapper}>
+      {Array.isArray(activeResults) &&
+        activeResults.length > 0 &&
+        activeResults.map((row, index) => {
+          return (
+            <Box
+              className={
+                index === activeSelectionIndex
+                  ? `${classes.resultRow} ${classes.activeRow}`
+                  : `${classes.resultRow}`
+              }
+              key={row.uniqueId}
+              onClick={() => handleRowClick(row)}
+              data-testid="select-directive-list-option"
+            >
+              <Box>
+                <Typography
+                  data-testid="select-directive-list-label"
+                  className={classes.directiveTitle}
+                  variant="body1"
+                >
+                  {row?.item?.directive || row?.item?.label}
+                </Typography>
+                <Typography
+                  data-testid="select-directive-list-description"
+                  className={classes.directiveDescription}
+                  variant="body1"
+                >
+                  {row?.item?.description}
+                </Typography>
+              </Box>
+            </Box>
+          );
+        })}
+    </Box>
+  );
+}
---
+++ app/cdap/components/DirectiveInput/iconStore.tsx
@@ -0,0 +1,39 @@
+/*
+ * Copyright © 2022 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+import React from 'react';
+
+export const CrossIcon = (
+  <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M0.352464 16.0727C-0.0380608 16.4632 -0.0380608 17.0964 0.352464 17.4869C0.742988 17.8774 1.37615 17.8774 1.76668 17.4869L8.73667 10.5169L15.7067 17.4869C16.0972 17.8774 16.7303 17.8774 17.1209 17.4869C17.5114 17.0964 17.5114 16.4632 17.1209 16.0727L10.1509 9.10271L17.1207 2.13289C17.5112 1.74236 17.5112 1.1092 17.1207 0.718674C16.7302 0.32815 16.097 0.32815 15.7065 0.718674L8.73667 7.6885L1.76685 0.718674C1.37632 0.32815 0.743156 0.32815 0.352632 0.718674C-0.0378928 1.1092 -0.0378924 1.74236 0.352632 2.13289L7.32245 9.10271L0.352464 16.0727Z"
+      fill="white"
+    />
+  </svg>
+);
+
+export const InfoIcon = (
+  <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <circle cx="10" cy="10" r="9" stroke="#79B7FF" stroke-width="2" />
+    <path d="M9.81006 9.15039V14.4138" stroke="#79B7FF" stroke-width="2" stroke-linecap="round" />
+    <path
+      d="M11.1093 5.68742C11.1093 6.37778 10.5497 6.93742 9.85933 6.93742C9.16898 6.93742 8.60934 6.37778 8.60934 5.68742C8.60934 4.99707 9.16898 4.43742 9.85933 4.43742C10.5497 4.43742 11.1093 4.99707 11.1093 5.68742Z"
+      fill="#79B7FF"
+    />
+  </svg>
+);
