a375c198b06cba1d4e6d372f237dcb7e43be718a
abhilashDivami <abhilash@divami.com>
2022-10-24 12:34:40 +0530

    fixed review commments

--- app/cdap/components/RecipeSteps/RecipeStepsEmptyScreen/index.tsx
+++ app/cdap/components/RecipeSteps/RecipeStepsEmptyScreen/index.tsx
@@ -13,6 +13,7 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
+
 import { Container } from '@material-ui/core';
 import React, { Fragment } from 'react';
 import { useStyles } from '../styles';
