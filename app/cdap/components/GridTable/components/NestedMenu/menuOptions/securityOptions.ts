6b7d809049311c66ef7530fdb7941b8984a10a02
Manisha <maneesha@divami.com>
2022-10-30 11:03:27 +0530

    Self review changes

--- app/cdap/components/GridTable/components/NestedMenu/menuOptions/securityOptions.ts
+++ app/cdap/components/GridTable/components/NestedMenu/menuOptions/securityOptions.ts
@@ -13,33 +13,34 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
+import T from 'i18n-react';
 
 export const SECURITY_OPTIONS = [
   {
     value: 'encode',
-    label: 'Encode',
+    label: `${T.translate('features.WranglerNewSecurityOptions.encode').toString()}`,
     options: [
       {
         value: 'encode-base64',
-        label: 'Base64',
+        label: `${T.translate('features.WranglerNewSecurityOptions.base64').toString()}`,
         supported_dataType: ['all'],
         directive: (columnSelected: string) => `encode base64 :${columnSelected}`,
       },
       {
         value: 'encode-base32',
-        label: 'Base32',
+        label: `${T.translate('features.WranglerNewSecurityOptions.base32').toString()}`,
         supported_dataType: ['all'],
         directive: (columnSelected: string) => `encode base32 :${columnSelected}`,
       },
       {
         value: 'encode-hex',
-        label: 'Hex',
+        label: `${T.translate('features.WranglerNewSecurityOptions.hex').toString()}`,
         supported_dataType: ['all'],
         directive: (columnSelected: string) => `encode hex :${columnSelected}`,
       },
       {
         value: 'encode-url',
-        label: 'URL',
+        label: `${T.translate('features.WranglerNewSecurityOptions.url').toString()}`,
         supported_dataType: ['all'],
         directive: (columnSelected: string) => `url-encode :${columnSelected}`,
       },
@@ -48,29 +49,29 @@ export const SECURITY_OPTIONS = [
   },
   {
     value: 'decode',
-    label: 'Decode',
+    label: `${T.translate('features.WranglerNewSecurityOptions.decode').toString()}`,
     options: [
       {
         value: 'decode-base64',
-        label: 'Base64',
+        label: `${T.translate('features.WranglerNewSecurityOptions.base64').toString()}`,
         supported_dataType: ['all'],
         directive: (columnSelected: string) => `decode base64 :${columnSelected}`,
       },
       {
         value: 'decode-base32',
-        label: 'Base32',
+        label: `${T.translate('features.WranglerNewSecurityOptions.base32').toString()}`,
         supported_dataType: ['all'],
         directive: (columnSelected: string) => `decode base32 :${columnSelected}`,
       },
       {
         value: 'decode-hex',
-        label: 'Hex',
+        label: `${T.translate('features.WranglerNewSecurityOptions.hex').toString()}`,
         supported_dataType: ['all'],
         directive: (columnSelected: string) => `decode hex :${columnSelected}`,
       },
       {
         value: 'decode-url',
-        label: 'URL',
+        label: `${T.translate('features.WranglerNewSecurityOptions.url').toString()}`,
         supported_dataType: ['all'],
         directive: (columnSelected: string) => `url-decode :${columnSelected}`,
       },
