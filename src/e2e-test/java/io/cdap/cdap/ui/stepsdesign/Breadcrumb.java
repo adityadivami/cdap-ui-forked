1c2be345a718ea06efce0475134f3fd972c8a1ed
Dhruti <dhruti@divami.com>
2022-11-01 15:11:43 +0530

    breadcrumb

--- src/e2e-test/java/io/cdap/cdap/ui/stepsdesign/Breadcrumb.java
+++ src/e2e-test/java/io/cdap/cdap/ui/stepsdesign/Breadcrumb.java
@@ -40,50 +40,69 @@ public class Breadcrumb {
         WaitHelper.waitForPageToLoad();
     }
 
-    @Then("Click on the Connector type")
-    public void clickOnTheConnectorType() {
-        WaitHelper.waitForPageToLoad();
-        ElementHelper.clickOnElement(Helper.locateElementByTestId("wranglecard-link-1"));
+    @Then("Click on the Connector type with \\\"(.*)\\\"  and \\\"(.*)\\\" ")
+    public void clickOnTheConnectorType(String connectionLabel,String connectionTestId) {
+        try {
+            ElementHelper.clickOnElement(Helper.locateElementByTestId("wrangle-card-" + connectionTestId));
+            System.out.println("Clicked on " + connectionLabel + " Element");
+            WaitHelper.waitForPageToLoad();
+            if (connectionLabel.equals("Add Connections")) {
+                ElementHelper.clickOnElement(Helper.locateElementByTestId("wrangle-card-" + connectionTestId));
+                System.out.println("Clicked on " + connectionLabel + " Element");
+                String ActualText = SeleniumDriver.getDriver().getCurrentUrl();
+                Assert.assertEquals(ActualText, "http://localhost:11011/cdap/ns/default/connections/create");
+                System.out.println("Navigated to " + connectionLabel + " Page - Old UI");
+            }
+            if (connectionLabel.equals("PostgreSQL") || connectionLabel.equals("File")) {
+                String ActualText = SeleniumDriver.getDriver().getCurrentUrl();
+                Assert.assertEquals(ActualText, "http://localhost:11011/cdap/ns/default/datasources/" + connectionLabel);
+                System.out.println("Navigated to Data Source page wit connection " + connectionLabel + " selected");
+            }
+        }catch(Exception e)
+        {
+            System.out.println(connectionLabel + " Element does not exist");
+        }
     }
-    @Then("click on the postgreSQL")
-    public void clickPostgreSql(){
 
-        WaitHelper.waitForPageToLoad();
-        ElementHelper.clickOnElement(Helper.locateElementByTestId("connectionlist-connectiontabs-tabs-loop-PostgreSQL"));
-    }
-
-
-    @Then("Click on the first tab of second column")
-    public void clickOnTheFirstTabOfSecondColumn() {
-        WaitHelper.waitForPageToLoad();
-        ElementHelper.clickOnElement(Helper.locateElementByTestId("connectionlist-connectiontabs-tabs-loop-EXL"));
-    }
-    @Then("Click on the first tab of third column")
-    public void clickOnTheFirstTabOfThirdColumn() {
-        WaitHelper.waitForPageToLoad();
-        ElementHelper.clickOnElement(Helper.locateElementByTestId("connectionlist-connectiontabs-tabs-loop-information_schema"));
-    }
-    @When("Hover&Click on the Wrangler")
-    public void hoverAndClickOnTheWrangler() {
-        WaitHelper.waitForPageToLoad();
-        WebElement ele = Helper.locateElementByTestId("connectionlist-connectiontabs-tabs-loop-sql_features");
-        Actions action = new Actions(SeleniumDriver.getDriver());
-        WaitHelper.waitForPageToLoad();
-        action.moveToElement(ele).perform();
-        WaitHelper.waitForPageToLoad();
-        ElementHelper.clickOnElement(Helper.locateElementByTestId("loadtogrid-wrangle-button"));
-    }
-    @Then("Click on the Data Sources link")
-    public void clickONTheDataSourcesLink() {
-        WaitHelper.waitForPageToLoad();
-        ElementHelper.clickOnElement(Helper.locateElementByTestId("breadcrumb-data-sources-text"));
-    }
-    @Then("Check the url is equal to Datasource url text")
-    public void dataSourcesText() {
-        WaitHelper.waitForPageToLoad();
-        String ActualText = SeleniumDriver.getDriver().getCurrentUrl();
-        Assert.assertEquals(ActualText, "http://localhost:11011/cdap/ns/default/datasources/Imported%20Datasets");
-    }
+//    @Then("click on the postgreSQL")
+//    public void clickPostgreSql(){
+//
+//        WaitHelper.waitForPageToLoad();
+//        ElementHelper.clickOnElement(Helper.locateElementByTestId("connectionlist-connectiontabs-tabs-loop-PostgreSQL"));
+//    }
+//
+//
+//    @Then("Click on the first tab of second column")
+//    public void clickOnTheFirstTabOfSecondColumn() {
+//        WaitHelper.waitForPageToLoad();
+//        ElementHelper.clickOnElement(Helper.locateElementByTestId("connectionlist-connectiontabs-tabs-loop-EXL"));
+//    }
+//    @Then("Click on the first tab of third column")
+//    public void clickOnTheFirstTabOfThirdColumn() {
+//        WaitHelper.waitForPageToLoad();
+//        ElementHelper.clickOnElement(Helper.locateElementByTestId("connectionlist-connectiontabs-tabs-loop-information_schema"));
+//    }
+//    @When("Hover&Click on the Wrangler")
+//    public void hoverAndClickOnTheWrangler() {
+//        WaitHelper.waitForPageToLoad();
+//        WebElement ele = Helper.locateElementByTestId("connectionlist-connectiontabs-tabs-loop-sql_features");
+//        Actions action = new Actions(SeleniumDriver.getDriver());
+//        WaitHelper.waitForPageToLoad();
+//        action.moveToElement(ele).perform();
+//        WaitHelper.waitForPageToLoad();
+//        ElementHelper.clickOnElement(Helper.locateElementByTestId("loadtogrid-wrangle-button"));
+//    }
+//    @Then("Click on the Data Sources link")
+//    public void clickONTheDataSourcesLink() {
+//        WaitHelper.waitForPageToLoad();
+//        ElementHelper.clickOnElement(Helper.locateElementByTestId("breadcrumb-data-sources-text"));
+//    }
+//    @Then("Check the url is equal to Datasource url text")
+//    public void dataSourcesText() {
+//        WaitHelper.waitForPageToLoad();
+//        String ActualText = SeleniumDriver.getDriver().getCurrentUrl();
+//        Assert.assertEquals(ActualText, "http://localhost:11011/cdap/ns/default/datasources/Imported%20Datasets");
+//    }
     @Then("Click on the Home link of wrangle page")
     public void clickOnTheHomeLink() {
         WaitHelper.waitForPageToLoad();
