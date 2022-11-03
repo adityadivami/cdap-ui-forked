7550aa3f97799b7e43527ecc55dbf9d0557f4129
richadivami <richa@divami.com>
2022-11-03 11:11:53 +0530

    Rebse

--- app/cdap/components/GridTable/components/TransformationComponents/index.tsx
+++ app/cdap/components/GridTable/components/TransformationComponents/index.tsx
@@ -24,7 +24,7 @@ export default function({
   transformationComponentValues,
   setTransformationComponentsValue,
   ...props
-}: IDirectiveContentParams) {
+}: ITransformationContentParams) {
   const Component = transformationComponent.find((item) => item.type === type)?.component;
 
   return (
