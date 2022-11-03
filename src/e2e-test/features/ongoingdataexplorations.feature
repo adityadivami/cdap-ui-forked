64647f359c3638766ac4caf1e62221b73b7988bf
Dhruti <dhruti@divami.com>
2022-11-03 22:53:48 +0530

    Ongoing data exploration cucumber changes

--- src/e2e-test/features/ongoingdataexplorations.feature
+++ src/e2e-test/features/ongoingdataexplorations.feature
@@ -3,7 +3,7 @@ Feature: Ongoing Data Explorations
 
   @OngoingDataExplorations
   Scenario: Go through the Ongoing Data Explorations Page
-    Given Navigate to the home page
-    Then Click on the Data Exploration
+    Given Navigate to the Home Page
+    Then Click on the Data Explorations Card
     Then Click on the Home link
-    Then Check the user is on the home page or not
+    Then Check if the user is on the Home Page
--- src/e2e-test/java/io/cdap/cdap/ui/stepsdesign/OngoingDataExplorations.java
+++ src/e2e-test/java/io/cdap/cdap/ui/stepsdesign/OngoingDataExplorations.java
@@ -10,14 +10,14 @@ import io.cucumber.java.en.Then;
 import org.junit.Assert;
 import org.openqa.selenium.WebElement;
 
-public class OngoingDataExploration {
-    @Given("Navigate to the home page")
+public class OngoingDataExplorations {
+    @Given("Navigate to the Home Page")
     public void navigateToTheHomePage(){
         SeleniumDriver.openPage(Constants.WRANGLE_HOME_URL);
         WaitHelper.waitForPageToLoad();
     }
-    @Then("Click on the Data Exploration")
-    public void clickOnTheDataExploration(){
+    @Then("Click on the Data Explorations Card")
+    public void clickOnTheDataExplorationsCard(){
         WaitHelper.waitForPageToLoad();
         WebElement ele = Helper.locateElementByTestId("home-ongoing-explorations-text-0");
         String homeText = ele.getText();
@@ -32,13 +32,13 @@ public class OngoingDataExploration {
         System.out.println("The file name is displayed correctly on grid page");
     }
     @Then("Click on the Home link")
-    public void clickONTheHomeLink(){
+    public void clickOnTheHomeLink(){
         WaitHelper.waitForPageToLoad();
         ElementHelper.clickOnElement(Helper.locateElementByTestId("breadcrumb-home-text"));
         System.out.println("the home link is clicked");
     }
-    @Then("Check the user is on the home page or not")
-    public void checkTheUserIsOnTheHomePageOrNot(){
+    @Then("Check if the user is on the Home Page")
+    public void checkIfTheUserIsOnTheHomePage(){
         String ActualText = SeleniumDriver.getDriver().getCurrentUrl();
         Assert.assertEquals(ActualText, "http://localhost:11011/cdap/ns/default/home");
         System.out.println("The assertion for home url is passed");
