c5c85e49edce30b4047a53cd7e9711f404e092d6
Dhruti <dhruti@divami.com>
2022-11-10 18:51:35 +0530

    final code

--- src/e2e-test/features/columninsightspanel.feature
+++ src/e2e-test/features/columninsightspanel.feature
@@ -1,11 +1,33 @@
+#
+# Copyright © 2022 Cask Data, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy of
+# the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
+#
+
 @Integration_Tests
 Feature: ColumnInsightsPanel
 
   @ADMIN_TEST
-  Scenario: Go through the Column Insights Panel
+  Scenario Outline: Go through the Column Insights Panel
     Given Navigate to Home Page
     Then Click on the Data Explorations card
     Then Click on any column from grid table
-    Then Verify if user changes the column name
-    Then Verify if user changes the data type
+    Then Verify if user changes the column name as "sudha"
     Then Click on the Cross icon
+    Then verify if edited name as "sudha" displayed for column
+    Then Click on any column from grid table
+    Then Verify if user changes the data type
+    Then verify if selected datatype is displayed for column as per "<id>"
+    Examples:
+    | id |
+    | 1 |
--- src/e2e-test/java/io/cdap/cdap/ui/stepsdesign/ColumnInsightsPanel.java
+++ src/e2e-test/java/io/cdap/cdap/ui/stepsdesign/ColumnInsightsPanel.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright © 2022 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package io.cdap.cdap.ui.stepsdesign;
 
 import io.cdap.cdap.ui.utils.Constants;
@@ -8,6 +24,7 @@ import io.cdap.e2e.utils.WaitHelper;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import org.junit.Assert;
+import org.openqa.selenium.WebElement;
 
 public class ColumnInsightsPanel {
     @Given("Navigate to Home Page")
@@ -30,27 +47,72 @@ public class ColumnInsightsPanel {
 
     @Then("Click on any column from grid table")
     public void clickOnAnyColumnFromGridTable() {
-        WaitHelper.waitForPageToLoad();
-        ElementHelper.clickOnElement(Helper.locateElementByTestId("grid-header-cell-body_0"));
+        try {
+            WaitHelper.waitForPageToLoad();
+            ElementHelper.clickOnElement(Helper.locateElementByTestId("grid-header-cell-0"));
+        } catch (Exception e) {
+            System.err.println("error:" + e);
+        }
     }
 
-    @Then("Verify if user changes the column name")
-    public void verifyIfUserChangesTheColumnName() {
-        WaitHelper.waitForPageToLoad();
-        ElementHelper.clickOnElement(Helper.locateElementByTestId("edit-icon"));
+    @Then("Verify if user changes the column name as {string}")
+    public void verifyIfUserChangesTheColumnName(String inputValue) {
+        try {
+            WaitHelper.waitForPageToLoad();
+            ElementHelper.clickOnElement(Helper.locateElementByTestId("edit-icon"));
+            Helper.locateElementByXPath("//input[@data-testid='column-name-edit-input']").click();
+            Helper.locateElementByXPath("//input[@data-testid='column-name-edit-input']").clear();
+            Helper.locateElementByXPath("//input[@data-testid='column-name-edit-input']").sendKeys(inputValue);
+            ElementHelper.clickOnElement(Helper.locateElementByTestId("edit-icon"));
+        } catch (Exception e) {
+            System.err.println("error:" + e);
+        }
+    }
+
+    @Then("verify if edited name as {string} displayed for column")
+    public void editedNameDisplayed(String inputValue) {
+        try {
+            String edited = Helper.locateElementByTestId("grid-header-cell-" + inputValue).getText();
+            Assert.assertEquals(edited, inputValue);
+        } catch (Exception e) {
+            System.err.println("error:" + e);
+        }
     }
 
     @Then("Verify if user changes the data type")
     public void verifyIfUserChangesTheDataType() {
-        WaitHelper.waitForPageToLoad();
-        ElementHelper.clickOnElement(Helper.locateElementByTestId("datatype-input-select"));
-        WaitHelper.waitForPageToLoad();
-        ElementHelper.clickOnElement(Helper.locateElementByTestId("input-select-1"));
+        try {
+            WaitHelper.waitForPageToLoad();
+            ElementHelper.clickOnElement(Helper.locateElementByTestId("datatype-input-select"));
+            WaitHelper.waitForPageToLoad();
+            ElementHelper.clickOnElement(Helper.locateElementByXPath("//ul/li[@data-testid='input-select-1']" ));
+        } catch (Exception e) {
+            System.err.println("error:" + e);
+        }
+    }
+
+    @Then("verify if selected datatype is displayed for column as per \\\"(.*)\\\"")
+    public void datatypeDisplayed(int id) {
+        try {
+            String text = Helper.locateElementByTestId("datatype-input-select").getText();
+            String edited = Helper.locateElementByTestId("input-select-" + id).getText();
+            if (edited.equals(text)) {
+                ElementHelper.clickOnElement(Helper.locateElementByTestId("drawer-widget-close-round-icon"));
+                String columnType = Helper.locateElementByTestId("typography-component-" + edited).getText();
+                Assert.assertEquals(columnType, edited);
+            }
+        } catch (Exception e) {
+            System.err.println("error:" + e);
+        }
     }
 
     @Then("Click on the Cross icon")
     public void clickOnTheCrossIcon() {
-        WaitHelper.waitForPageToLoad();
-        ElementHelper.clickOnElement(Helper.locateElementByTestId("drawer-widget-close-round-icon"));
+        try {
+            WaitHelper.waitForPageToLoad();
+            ElementHelper.clickOnElement(Helper.locateElementByTestId("drawer-widget-close-round-icon"));
+        } catch (Exception e) {
+            System.err.println("error:" + e);
+        }
     }
 }
