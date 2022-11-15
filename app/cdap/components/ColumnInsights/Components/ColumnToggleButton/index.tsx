d3e23cd8476ab5ffaf031f0d41d533f6d88d6115
smritiDivami <smriti@divami.com>
2022-11-15 20:56:24 +0530

    Styled Components

--- app/cdap/components/ColumnInsights/Components/ColumnToggleButton/index.tsx
+++ app/cdap/components/ColumnInsights/Components/ColumnToggleButton/index.tsx
@@ -15,11 +15,12 @@
  */
 
 import { Box } from '@material-ui/core';
-import { useStyles } from 'components/ColumnInsights/Components/ColumnToggleButton/styles';
+import RenderLabel from 'components/ColumnInsights/Components/common/RenderLabel/';
 import T from 'i18n-react';
 import React, { useState } from 'react';
 import styled from 'styled-components';
-import ToggleButton from 'components/ColumnInsights/Components/ToggleButton';
+import { Button } from '@material-ui/core';
+import { blue } from '@material-ui/core/colors';
 
 const PREFIX = 'features.NewWranglerUI.ColumnInsights';
 interface IDataQualityProps {
@@ -34,44 +35,101 @@ interface IDataQuality {
 }
 
 const StyledDataQualityBox = styled(Box)`
-    &&& { 
-      width: 100%;
-      display: flex;
-      align-items: center;
-      background-color: rgb(255, 255, 255);
-      justify-content: center
-      filter : drop-shadow(0px 2px 4px rgba(68, 132, 245, 0.25));
-      border-radius: 4px;
-      margin-top: 20px;
-    }
+  width: 100%;
+  display: flex;
+  align-items: center;
+  background-color: rgb(255, 255, 255);
+  justify-content: center;
+  filter: drop-shadow(0px 2px 4px rgba(68, 132, 245, 0.25));
+  border-radius: 4px;
+  margin-top: 20px;
 `;
 
+const StyledToggleButton = styled(Button)`
+  border: 1px solid #dadce0;
+  width: 50%;
+  text-align: center;
+  padding: 10px;
+  cursor: pointer;
+  text-transform: none;
+`;
+
+const StyledToggleLeftButton = styled(StyledToggleButton)`
+  border-top-left-radius: 4px;
+  border-bottom-left-radius: 4px;
+  border-top-right-radius: 0px;
+  border-bottom-right-radius: 0px;
+  border-right: 1px solid #dadce0;
+`;
+
+const SelectedToogleLeftButton = styled(StyledToggleLeftButton)`
+  background: #f3f6f9;
+  border: 1px solid ${blue[500]} !important;
+  box-shadow: inset 2px 2px 2px rgba(68, 132, 245, 0.4);
+`;
+
+const StyledToggleRightButton = styled(StyledToggleButton)`
+  border-top-right-radius: 4px;
+  border-bottom-right-radius: 4px;
+  border-top-left-radius: 0px;
+  border-bottom-left-radius: 0px;
+  border-left: 0px;
+  text-transform: none;
+`;
+
+const SelectedToogleRightButton = styled(StyledToggleRightButton)`
+  background: #f3f6f9;
+  border: 1px solid ${blue[500]} !important;
+  box-shadow: inset 2px 2px 2px rgba(68, 132, 245, 0.4);
+`;
+
+const getWrapperComponent = (isSelected, position) => {
+  if (position === 'left') {
+    if (isSelected === 1) {
+      return SelectedToogleLeftButton;
+    } else {
+      return StyledToggleLeftButton;
+    }
+  } else {
+    if (isSelected === 2) {
+      return SelectedToogleRightButton;
+    } else {
+      return StyledToggleRightButton;
+    }
+  }
+};
+
 export default function({ dataQuality }: IDataQualityProps) {
-  const classes = useStyles();
   const [isSelected, setIsSelected] = useState<number>(0);
+
+  const LeftWrapperComponent = getWrapperComponent(isSelected, 'left');
+  const RightWrapperComponent = getWrapperComponent(isSelected, 'right');
   return (
     <StyledDataQualityBox data-testid={'data-quality-toggle-parent'}>
-      <ToggleButton
-        type={'left'}
+      <LeftWrapperComponent
         setIsSelected={setIsSelected}
-        className={isSelected === 1 ? classes.isSelected : ''}
+        onClick={() => setIsSelected(1)}
+        data-testid={`toggle-button-left`}
       >
-        <>
-          {T.translate(`${PREFIX}.empty`).toString()}
-          {` ${dataQuality.emptyValueCount} (${dataQuality.emptyValuePercentage}%)`}
-        </>
-      </ToggleButton>
-
-      <ToggleButton
-        type={'right'}
+        <RenderLabel fontSize={14}>
+          <>
+            {T.translate(`${PREFIX}.empty`).toString()}
+            {` ${dataQuality.emptyValueCount} (${dataQuality.emptyValuePercentage}%)`}
+          </>
+        </RenderLabel>
+      </LeftWrapperComponent>
+      <RightWrapperComponent
         setIsSelected={setIsSelected}
-        className={isSelected === 2 ? classes.isSelected : ''}
+        onClick={() => setIsSelected(2)}
+        data-testid={`toggle-button-right`}
       >
-        <>
-          {T.translate(`${PREFIX}.null`)}
-          {` ${dataQuality?.nullValueCount} (${dataQuality?.nullValuePercentage}%)`}
-        </>
-      </ToggleButton>
+        <RenderLabel fontSize={14}>
+          <>
+            {T.translate(`${PREFIX}.null`)}
+            {` ${dataQuality?.nullValueCount} (${dataQuality?.nullValuePercentage}%)`}
+          </>
+        </RenderLabel>
+      </RightWrapperComponent>
     </StyledDataQualityBox>
   );
 }
