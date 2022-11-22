51b4ba8f95851509a67da6aa257a99b714f2b656
smritiDivami <smriti@divami.com>
2022-11-22 21:51:12 +0530

    API unit testing

--- app/cdap/components/FunctionSearch/__test__/FunctionSearch.test.tsx
+++ app/cdap/components/FunctionSearch/__test__/FunctionSearch.test.tsx
@@ -17,7 +17,70 @@
 import React from 'react';
 import { fireEvent, render, screen } from '@testing-library/react';
 import FunctionSearch from 'components/FunctionSearch';
+import MyDataPrepApi from 'api/dataprep';
+
 describe('It Should test FunctionSeach Component.', () => {
+  beforeEach(() => {
+    jest.spyOn(MyDataPrepApi, 'getUsage').mockImplementation(() => {
+      return {
+        subscribe: (callback) => {
+          callback({
+            message: 'Success',
+            count: 84,
+            values: [
+              {
+                directive: 'encode',
+                usage: "encode 'method' :column",
+                description: 'Encodes column values using one of base32, base64, or hex.',
+                excluded: false,
+                alias: false,
+                scope: 'SYSTEM',
+                arguments: {
+                  directive: 'encode',
+                  tokens: [
+                    {
+                      ordinal: 0,
+                      optional: false,
+                      name: 'method',
+                      type: 'TEXT',
+                    },
+                    {
+                      ordinal: 1,
+                      optional: false,
+                      name: 'column',
+                      type: 'COLUMN_NAME',
+                    },
+                  ],
+                },
+                categories: ['transform'],
+              },
+              {
+                directive: 'uppercase',
+                usage: 'uppercase :column',
+                description: 'Changes the column values to uppercase.',
+                excluded: false,
+                alias: false,
+                scope: 'SYSTEM',
+                arguments: {
+                  directive: 'uppercase',
+                  tokens: [
+                    {
+                      ordinal: 0,
+                      optional: false,
+                      name: 'column',
+                      type: 'COLUMN_NAME',
+                    },
+                  ],
+                },
+                categories: ['transform'],
+              },
+            ],
+          });
+        },
+      };
+    });
+  });
+
   it('Should test whether FunctionSearch Component is rendered .', () => {
     render(<FunctionSearch transformationPanel={jest.fn()} />);
     const searchBox = screen.getByTestId(/search-box/i);
@@ -44,5 +107,16 @@ describe('It Should test FunctionSeach Component.', () => {
 
     const clearIcon = screen.getByTestId(/clear-search-icon/i);
     fireEvent.click(clearIcon);
+  });
+
+  it('It should search and click on uppercase directive.', () => {
+    const { container } = render(<FunctionSearch transformationPanel={jest.fn()} />);
+    const searchInputField = container.querySelector('input');
+
+    fireEvent.change(searchInputField, { target: { value: 'uppercase' } });
+    expect(searchInputField).toHaveValue('uppercase');
+    const uppercase = screen.getByTestId(/search-result-uppercase/i);
+    fireEvent.click(uppercase);
+    expect(uppercase).toBeInTheDocument();
   });
 });
