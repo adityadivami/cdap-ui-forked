5c21793778da6fb7a0925c5aa83b54d8daf5aeb7
sudhadivami <sudhalavanya@divami.com>
2022-11-23 15:14:19 +0530

    order changed

--- src/e2e-test/java/io/cdap/cdap/ui/stepsdesign/CalculateTransformation.java
+++ src/e2e-test/java/io/cdap/cdap/ui/stepsdesign/CalculateTransformation.java
@@ -1,11 +1,11 @@
 package io.cdap.cdap.ui.stepsdesign;
 
+import io.cdap.cdap.ui.utils.Constants;
 import io.cdap.cdap.ui.utils.Helper;
 import io.cdap.e2e.utils.ElementHelper;
-import io.cucumber.java.en.Given;
 import io.cdap.e2e.utils.SeleniumDriver;
-import io.cdap.cdap.ui.utils.Constants;
 import io.cdap.e2e.utils.WaitHelper;
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import org.junit.Assert;
 import org.openqa.selenium.JavascriptExecutor;
