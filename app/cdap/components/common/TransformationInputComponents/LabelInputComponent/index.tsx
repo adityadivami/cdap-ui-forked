1f40f7a4e3dece93aa4355ad22c17ac4ed9e5164
smritiDivami <smriti@divami.com>
2022-11-25 12:29:13 +0530

    Find and Replace

--- app/cdap/components/common/TransformationInputComponents/LabelInputComponent/index.tsx
+++ app/cdap/components/common/TransformationInputComponents/LabelInputComponent/index.tsx
@@ -15,20 +15,13 @@
  */
 
 import React from 'react';
+import { NormalFont } from 'components/common/TypographyText';
+import { LabelWrapper } from 'components/common/BoxContainer';
 
-// This image is an SVG and is not available in MUI Icons Store
-export const Underline = () => {
+export default function({ labelText }: { labelText: string }) {
   return (
-    <svg
-      width="67"
-      height="3"
-      viewBox="0 0 67 3"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      data-testid="underline-icon"
-    >
-      <path d="M0 0.530273H50L53 2.5318H3L0 0.530273Z" fill="#2196F3" />
-      <path d="M54 0.530273H63.5L66.5 2.5318H57L54 0.530273Z" fill="#2196F3" />
-    </svg>
+    <LabelWrapper>
+      <NormalFont>{labelText}</NormalFont>
+    </LabelWrapper>
   );
-};
+}
