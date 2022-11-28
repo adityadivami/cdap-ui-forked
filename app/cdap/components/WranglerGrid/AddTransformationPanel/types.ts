7e56f8731a2aab543746083dfee478ba5cdb0dae
smritiDivami <smriti@divami.com>
2022-11-28 12:43:34 +0530

    Define Variable Common Code

--- app/cdap/components/WranglerGrid/AddTransformationPanel/types.ts
+++ app/cdap/components/WranglerGrid/AddTransformationPanel/types.ts
@@ -14,21 +14,22 @@
  * the License.
  */
 
-import React from 'react';
+import { IStatistics } from 'components/GridTable/types';
+import { IHeaderNamesList } from 'components/WranglerGrid/SelectColumnPanel/types';
 
-// This image is an SVG and is not available in MUI Icons Store
-export const Underline = () => {
-  return (
-    <svg
-      width="67"
-      height="3"
-      viewBox="0 0 67 3"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      data-testid="underline-icon"
-    >
-      <path d="M0 0.530273H50L53 2.5318H3L0 0.530273Z" fill="#2196F3" />
-      <path d="M54 0.530273H63.5L66.5 2.5318H57L54 0.530273Z" fill="#2196F3" />
-    </svg>
-  );
-};
+export interface ITransformationComponentValues {
+  customInput: string;
+  ignoreCase: boolean;
+  filterOptionSelected: string;
+  filterOptionValue: string;
+  filterRadioOption: string;
+}
+
+export interface IAddTransformationProps {
+  transformationDataType: string[];
+  transformationName: string;
+  columnsList: IHeaderNamesList[];
+  missingItemsList: IStatistics;
+  onCancel: () => void;
+  applyTransformation: (directive: string) => void;
+}
