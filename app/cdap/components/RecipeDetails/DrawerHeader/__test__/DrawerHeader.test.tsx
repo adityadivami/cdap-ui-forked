1684791fda404bf6d282440d17e1672fe40c5e0f
richadivami <richa@divami.com>
2022-12-06 19:31:58 +0530

    Saved Recipe

---
+++ app/cdap/components/RecipeDetails/DrawerHeader/__test__/DrawerHeader.test.tsx
@@ -0,0 +1,30 @@
+/*
+ *  Copyright © 2022 Cask Data, Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ *  use this file except in compliance with the License. You may obtain a copy of
+ *  the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ *  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ *  License for the specific language governing permissions and limitations under
+ *  the License.
+ */
+
+import { screen } from '@testing-library/dom';
+import { fireEvent, render } from '@testing-library/react';
+import RecipeDrawerHeader from 'components/RecipeDetails/DrawerHeader';
+import React from 'react';
+
+describe('Test RecipeDrawerHeader Component', () => {
+  it('should render the RecipeDrawerHeader Component ', () => {
+    render(<RecipeDrawerHeader onCloseDetail={jest.fn()} />);
+
+    const closePanel = screen.getByTestId(/close-recipe-detail/i);
+    fireEvent.click(closePanel);
+    expect(closePanel).toBeInTheDocument();
+  });
+});
