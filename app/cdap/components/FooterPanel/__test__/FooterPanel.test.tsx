54e2579f1c89fc4a22ba70cd119c66a4836c21c7
abhilashDivami <abhilash@divami.com>
2022-12-06 12:10:08 +0530

    UT Fixes

--- app/cdap/components/FooterPanel/__test__/FooterPanel.test.tsx
+++ app/cdap/components/FooterPanel/__test__/FooterPanel.test.tsx
@@ -45,9 +45,7 @@ describe('Test Footer Panel Component', () => {
 
     // Check if the row count and column count is same as provided props 66 and 6
     expect(simpleLabelElement[0]).toHaveTextContent(
-      `${T.translate(`${PREFIX}.currentData`)} - 66 ${T.translate(`${PREFIX}.rows`)} ${T.translate(
-        `features.WranglerNewUI.common.and`
-      )} 6 ${T.translate(`${PREFIX}.columns`)}`
+      `${T.translate(`${PREFIX}.message`, 66)}`
     );
   });
 
