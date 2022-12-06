05b052e69aa57986dac9cf42eefae6a6792fbd68
richadivami <richa@divami.com>
2022-12-06 12:31:23 +0530

    Saved Recipe

---
+++ app/cdap/components/RecipeDetails/index.tsx
@@ -0,0 +1,43 @@
+/*
+ *  Copyright © 2022 Cask Data, Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ *  use this file except in compliance with the License. You may obtain a copy of
+ *  the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ *  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ *  License for the specific language governing permissions and limitations under
+ *  the License.
+ */
+
+import React from 'react';
+import styled from 'styled-components';
+import { Container, Drawer } from '@material-ui/core';
+
+const StyledDrawer = styled(Drawer)`
+  & .MuiDrawer-paper {
+    top: 46px;
+    height: calc(100vh - 47px);
+  }
+`;
+
+const DrawerContainerBox = styled(Container)`
+  width: 460px;
+  height: 100%;
+  padding-left: 30px;
+`;
+
+export default function({}) {
+  return (
+    <StyledDrawer open={true} data-testid="select-column-panel" anchor="right">
+      <DrawerContainerBox
+        role="presentation"
+        data-testid="select-column-drawer"
+      ></DrawerContainerBox>
+    </StyledDrawer>
+  );
+}
