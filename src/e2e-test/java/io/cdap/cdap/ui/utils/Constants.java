1a9d744ab039f48b9cca86123457889faa6f3fe7
aditya <aditya@divami.com>
2022-12-07 17:25:41 +0530

    CDAP-19817 footer panel CT fixes

--- src/e2e-test/java/io/cdap/cdap/ui/utils/Constants.java
+++ src/e2e-test/java/io/cdap/cdap/ui/utils/Constants.java
@@ -51,7 +51,7 @@ public class Constants {
   public static final String DEFAULT_GCS_FILE = "purchase_bad.csv";
 
   public static final String DEFAULT_BIGQUERY_CONNECTION_NAME = "bigquery_" +
-      String.valueOf(getRandomArbitrary(1, 10000));
+    String.valueOf(getRandomArbitrary(1, 10000));
   public static final String DEFAULT_BIGQUERY_DATASET = "cdap_gcp_ui_test";
   public static final String DEFAULT_BIGQUERY_TABLE = "users";
 
@@ -59,7 +59,7 @@ public class Constants {
   public static final String DEFAULT_SPANNER_DATABASE = "test";
   public static final String DEFAULT_SPANNER_TABLE = "users";
   public static final String DEFAULT_SPANNER_CONNECTION_NAME = "spanner_" +
-      String.valueOf(getRandomArbitrary(1, 10000));
+    String.valueOf(getRandomArbitrary(1, 10000));
 
   public static final String RUNTIME_ARGS_PREVIEW_SELECTOR = "runtimeargs-preview";
   public static final String RUNTIME_ARGS_DEPLOYED_SELECTOR = "runtimeargs-deployed";
