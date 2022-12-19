b4c999845cfac2e138cd084773f82d45bfdc3ca8
aditya <aditya@divami.com>
2022-12-19 09:34:59 +0530

    CDAP-20056 Edit Recipe

---
+++ app/cdap/components/EditRecipe/__test__/EditRecipeForm.test.tsx
@@ -0,0 +1,41 @@
+/*
+ *  Copyright © 2022 Cask Data, Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ *  use this file except in compliance with the License. You may obtain a copy of
+ *  the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ *  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ *  License for the specific language governing permissions and limitations under
+ *  the License.
+ */
+
+import { render, screen, fireEvent } from '@testing-library/react';
+import React from 'react';
+import EditRecipe from 'components/EditRecipe';
+import { ISnackbar } from 'components/Snackbar';
+
+describe('Test Create Recipe Component', () => {
+  const mockSetSnackbar = jest.fn();
+  beforeEach(() => {
+    render(
+      <EditRecipe
+        recipeData={{ recipeName: 'recipeName', description: '', directives: [] }}
+        headingText={''}
+        openDrawer={true}
+        onCloseClick={jest.fn()}
+        setSnackbar={mockSetSnackbar}
+        setRecipeFormOpen={mockSetSnackbar}
+      />
+    );
+  });
+
+  it('should render the component as expected', () => {
+    const parentElement = screen.getByTestId(/edit-recipe-drawer-widget-parent/i);
+    expect(parentElement).toBeInTheDocument();
+  });
+});
