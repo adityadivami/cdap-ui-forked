15eab9c4f9a236e8c2e36dc142ca6a12a9e6cdf5
aditya <aditya@divami.com>
2023-01-11 15:24:53 +0530

    CDAP-19706 added copyright in styles.ts

--- app/cdap/components/InlayDrawerWidget/styles.ts
+++ app/cdap/components/InlayDrawerWidget/styles.ts
@@ -1,4 +1,20 @@
-import { Box, Button, IconButton, Menu, MenuItem, Typography } from '@material-ui/core';
+/*
+ * Copyright © 2023 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+import { Box, Button, IconButton, MenuItem, Typography } from '@material-ui/core';
 import grey from '@material-ui/core/colors/grey';
 import styled from 'styled-components';
 
