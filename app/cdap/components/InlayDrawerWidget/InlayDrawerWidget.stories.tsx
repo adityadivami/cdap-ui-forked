43d2176646d46117947d200de30b10d4fe6c5960
aditya <aditya@divami.com>
2023-01-17 11:05:55 +0530

    CDAP-19706 removed args from default export in story book

--- app/cdap/components/InlayDrawerWidget/InlayDrawerWidget.stories.tsx
+++ app/cdap/components/InlayDrawerWidget/InlayDrawerWidget.stories.tsx
@@ -21,9 +21,6 @@ import React from 'react';
 export default {
   title: 'InlayDrawerWidget',
   component: InlayDrawerWidget,
-  argTypes: {
-    onClose: { control: { type: 'radio' } },
-  },
 };
 
 const handleDrawerCloseIconClick = () => action('clicked')('Drawer Closed');
